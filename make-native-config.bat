dd5bf4809440b391db08ecbf870bbb94c637baba
Ghost_chu <30802565+Ghost-chu@users.noreply.github.com>
2024-03-27 13:39:57 +0800

    打包时指定固定运行于英语美国环境，以修复不同语言系统下 NativeImage 的打包文件缺失语言文件的问题

--- make-native-config.bat
+++ make-native-config.bat
@@ -1,3 +1,3 @@
 @echo off
 cd target
-java -agentlib:native-image-agent=config-output-dir=../src/main/resources/META-INF/native-image -jar PeerBanHelper.jar
+java -agentlib:native-image-agent=config-merge-dir=../src/main/resources/META-INF/native-image -Duser.language=en -Duser.region=US -jar PeerBanHelper.jar
