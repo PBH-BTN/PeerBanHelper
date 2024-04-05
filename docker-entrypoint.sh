98b57c0943e8ccf683287975eef3b22fcbf6eca3
Ghost_chu <ghostchu@111.com>
2024-04-05 18:12:36 +0800

    允许使用环境变量启动native image

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-java -Xmx256M -XX:+UseG1GC -XX:+UseStringDeduplication -jar PeerBanHelper.jar
+if [ -z $USE_NATIVE_IMAGE ]
+then
+  echo "The environment variable USE_NATIVE_IMAGE must be set."
+fi
+
+if [ $USE_NATIVE_IMAGE -eq 1 ]
+then
+  chmod +x peerbanhelper-binary
+  ./peerbanhelper-binary
+else
+  java -Xmx256M -XX:+UseSerialGC -jar PeerBanHelper.jar
+fi
