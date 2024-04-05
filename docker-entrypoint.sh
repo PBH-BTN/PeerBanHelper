565bdd11c7ef2bd62aeb9857ffa37604fabab07b
Ghost_chu <ghostchu@111.com>
2024-04-05 19:57:51 +0800

    准备发版

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ if [ "$USE_NATIVE_IMAGE" -eq 1 ]
 then
   if [ -f "$BIN_FILE" ]
   then
-      echo "$BIN_FILE exist"
+      echo "This image supports native-image, add USE_NATIVE_IMAGE=1 to use it (maybe buggy but saved a lots of system resources)"
   else
     echo "PeerBanHelper binary file not exists but USE_NATIVE_IMAGE=1, please disable native image option or use native-image included image"
     exit 1
