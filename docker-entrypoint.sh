9cde5e3bdb24d0aa5ddd487ae523d4df54384130
Ghost_chu <ghostchu@111.com>
2024-04-05 18:52:01 +0800

    分离 native-image 的 dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+if [ -z $NATIVE_SUPPORT ]
+then
+  echo "The environment variable NATIVE_SUPPORT must be set."
+fi
+
 if [ -z $USE_NATIVE_IMAGE ]
 then
   echo "The environment variable USE_NATIVE_IMAGE must be set."
@@ -6,8 +12,13 @@ fi
 
 if [ $USE_NATIVE_IMAGE -eq 1 ]
 then
-  chmod +x peerbanhelper-binary
-  ./peerbanhelper-binary
+  if [ $NATIVE_SUPPORT -eq 1 ]
+  then
+    chmod +x peerbanhelper-binary
+    ./peerbanhelper-binary
+  else
+    echo "This PeerBanHelper image aren't included the native image."
+  fi
 else
   java -Xmx256M -XX:+UseSerialGC -jar PeerBanHelper.jar
 fi
