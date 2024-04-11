1963e649e1f8565fbb42e3dd56532a99b3d56cf4
Ghost_chu <ghostchu@111.com>
2024-04-12 00:30:44 +0800

    修复启动脚本的提示

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,15 +4,18 @@ if [ "$USE_NATIVE_IMAGE" -eq 1 ]
 then
   if [ -f "$BIN_FILE" ]
   then
-      echo "This image supports native-image, add USE_NATIVE_IMAGE=1 to use it (maybe buggy but saved a lots of system resources)"
+      echo "Launching PeerBanHelper via Native-Image binary file..."
+      chmod +x $BIN_FILE
+      ./peerbanhelper-binary
   else
     echo "PeerBanHelper binary file not exists but USE_NATIVE_IMAGE=1, please disable native image option or use native-image included image"
     exit 1
   fi
-  echo "Launching PeerBanHelper via Native-Image binary file..."
-  chmod +x $BIN_FILE
-  ./peerbanhelper-binary
 else
   echo "Launching PeerBanHelper via universal JAR..."
+  if [ -f "$BIN_FILE" ]
+  then
+    echo "This image supports native-image, add USE_NATIVE_IMAGE=1 to use it (maybe buggy but saved a lots of system resources)"
+  fi
   java -Xmx256M -XX:+UseSerialGC -jar PeerBanHelper.jar
 fi
