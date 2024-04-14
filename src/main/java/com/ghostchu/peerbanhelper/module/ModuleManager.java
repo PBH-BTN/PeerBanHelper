0b2e4a5fcb4750e579037e9a4c5006e80943490f
Ghost_chu <ghostchu@111.com>
2024-04-14 22:24:43 +0800

    在模块被禁用时，不要启用它

--- src/main/java/com/ghostchu/peerbanhelper/module/ModuleManager.java
+++ src/main/java/com/ghostchu/peerbanhelper/module/ModuleManager.java
@@ -6,23 +6,28 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @Slf4j
 public class ModuleManager {
     private final List<FeatureModule> modules = new ArrayList<>();
 
     /**
      * 注册一个功能模块并启用它
+     *
      * @param module 功能模块
      */
     public void register(@NotNull FeatureModule module) {
         synchronized (modules) {
             this.modules.add(module);
-            module.enable();
+            if (module.isModuleEnabled()) {
+                module.enable();
+            }
         }
     }
 
     /**
      * 解注册一个功能模块、禁用它并清理注册的资源
+     *
      * @param module 功能模块
      * @return 该功能模块成功解注册
      */
@@ -35,6 +40,7 @@ public class ModuleManager {
 
     /**
      * 解注册给定的 Class 的所有功能模块实例
+     *
      * @param module 功能模块类
      */
     public void unregister(@NotNull Class<FeatureModule> module) {
@@ -58,6 +64,7 @@ public class ModuleManager {
 
     /**
      * 获取注册的所有功能模块
+     *
      * @return 功能模块列表（不可修改）
      */
     @NotNull
