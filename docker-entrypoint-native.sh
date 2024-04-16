d90641e35dfa861c7a4ea837494067e7655f963a
Gaojianli <admin@gaojianli.me>
2024-04-16 14:07:45 +0800

    native image don't need jre

---
+++ docker-entrypoint-native.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+BIN_FILE=/app/peerbanhelper-binary
+echo "Launching PeerBanHelper via Native-Image binary file..."
+exec $BIN_FILE
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,21 +1,2 @@
 #!/bin/sh
-BIN_FILE=/app/peerbanhelper-binary
-if [ "$USE_NATIVE_IMAGE" -eq 1 ]
-then
-  if [ -f "$BIN_FILE" ]
-  then
-      echo "Launching PeerBanHelper via Native-Image binary file..."
-      chmod +x $BIN_FILE
-      exec $BIN_FILE
-  else
-    echo "PeerBanHelper binary file not exists but USE_NATIVE_IMAGE=1, please disable native image option or use native-image included image"
-    exit 1
-  fi
-else
-  echo "Launching PeerBanHelper via universal JAR..."
-  if [ -f "$BIN_FILE" ]
-  then
-    echo "This image supports native-image, add USE_NATIVE_IMAGE=1 to use it (maybe buggy but saved a lots of system resources)"
-  fi
-  exec java -Xmx256M -XX:+UseSerialGC -jar PeerBanHelper.jar
-fi
+exec java -Xmx256M -XX:+UseSerialGC -jar PeerBanHelper.jar
