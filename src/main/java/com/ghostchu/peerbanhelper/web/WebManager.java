b5954c5703d8dde1474ebd30045be7251c6a54ee
Ghost_chu <ghostchu@111.com>
2024-04-25 22:39:59 +0800

    默认对 404 资源返回 index.html

--- src/main/java/com/ghostchu/peerbanhelper/web/WebManager.java
+++ src/main/java/com/ghostchu/peerbanhelper/web/WebManager.java
@@ -76,9 +76,16 @@ public class WebManager extends NanoHTTPD {
         if (uri.isBlank() || uri.equals("/")) {
             uri = "/index.html";
         }
+        return tryServeResources(session, uri);
+    }
+
+    private Response tryServeResources(IHTTPSession session, String uri) {
         InputStream is = getClass().getResourceAsStream("/static" + uri);
         if (is == null) {
-            return newFixedLengthResponse(Response.Status.NOT_FOUND, "text/plain", "Error: Resources not found");
+            if (uri.equals("/index.html")) {
+                return newFixedLengthResponse(Response.Status.NOT_FOUND, "text/plain", "Error: Resources not found");
+            }
+            return tryServeResources(session, "/index.html");
         }
         return newChunkedResponse(Response.Status.OK, NanoHTTPD.getMimeTypeForFile(uri), is);
     }
