cb2906b61b0f25d3322ccd9f51dd0928c5d0a52a
PluieM <644202913@qq.com>
2024-05-14 16:23:53 +0800

    1、调整部分代码符合规范 2、使用线程池调度订阅更新 3、使用guava生成sha256减少非必要的依赖

--- src/main/java/com/ghostchu/peerbanhelper/module/impl/rule/IPBlackRuleList.java
+++ src/main/java/com/ghostchu/peerbanhelper/module/impl/rule/IPBlackRuleList.java
@@ -2,8 +2,6 @@ package com.ghostchu.peerbanhelper.module.impl.rule;
 
 import cn.hutool.core.date.StopWatch;
 import cn.hutool.core.io.FileUtil;
-import cn.hutool.crypto.digest.DigestAlgorithm;
-import cn.hutool.crypto.digest.Digester;
 import com.ghostchu.peerbanhelper.Main;
 import com.ghostchu.peerbanhelper.PeerBanHelperServer;
 import com.ghostchu.peerbanhelper.module.AbstractRuleFeatureModule;
@@ -13,12 +11,12 @@ import com.ghostchu.peerbanhelper.peer.Peer;
 import com.ghostchu.peerbanhelper.text.Lang;
 import com.ghostchu.peerbanhelper.torrent.Torrent;
 import com.ghostchu.peerbanhelper.util.HTTPUtil;
+import com.ghostchu.peerbanhelper.util.IPAddressUtil;
 import com.ghostchu.peerbanhelper.util.rule.MatchResult;
 import com.ghostchu.peerbanhelper.util.rule.matcher.IPBanMatcher;
-import com.github.mizosoft.methanol.Methanol;
 import com.github.mizosoft.methanol.MutableRequest;
+import com.google.common.hash.Hashing;
 import inet.ipaddr.IPAddress;
-import inet.ipaddr.IPAddressString;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.bspfsystems.yamlconfiguration.configuration.ConfigurationSection;
@@ -26,17 +24,15 @@ import org.bspfsystems.yamlconfiguration.file.YamlConfiguration;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
-import java.net.CookieManager;
-import java.net.CookiePolicy;
-import java.net.http.HttpClient;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
-import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * IP黑名单远程订阅模块
@@ -45,13 +41,10 @@ import java.util.concurrent.ExecutorService;
 @Slf4j
 public class IPBlackRuleList extends AbstractRuleFeatureModule {
     private List<IPBanMatcher> bannedIps;
-    private HttpClient httpClient;
     private long checkInterval = 86400000; // 默认24小时检查一次
-    private long lastCheckTime = 0;
 
     public IPBlackRuleList(PeerBanHelperServer server, YamlConfiguration profile) {
         super(server, profile);
-        setupHttpClient();
     }
 
     @Override
@@ -71,31 +64,30 @@ public class IPBlackRuleList extends AbstractRuleFeatureModule {
 
     @Override
     public @NotNull BanResult shouldBanPeer(@NotNull Torrent torrent, @NotNull Peer peer, @NotNull ExecutorService ruleExecuteExecutor) {
-        if (System.currentTimeMillis() - lastCheckTime > checkInterval) {
-            // 若超过检查间隔则重新加载配置
-            reloadConfig();
-        }
         StopWatch watch = StopWatch.create("timer");
         watch.start("t1");
         String ip = peer.getAddress().getIp();
-        List<CompletableFuture<MatchResult>> fetchPeerFutures = new ArrayList<>(bannedIps.size());
-        bannedIps.forEach(rule -> fetchPeerFutures.add(CompletableFuture.supplyAsync(() -> rule.match(ip))));
+        List<CompletableFuture<IPBanResult>> fetchPeerFutures = new ArrayList<>(bannedIps.size());
+        bannedIps.forEach(rule -> fetchPeerFutures.add(CompletableFuture.supplyAsync(() -> new IPBanResult(rule.getRuleName(), rule.match(ip)))));
         CompletableFuture.allOf(fetchPeerFutures.toArray(new CompletableFuture[0])).join();
-        BanResult result = null;
-        try {
-            for (int i = 0; i < fetchPeerFutures.size(); i++) {
-                if (fetchPeerFutures.get(i).get() == MatchResult.TRUE) {
-                    result = new BanResult(this, PeerAction.BAN, ip, String.format(Lang.MODULE_IBL_MATCH_IP_RULE, bannedIps.get(i).getRuleName()));
-                    break;
+        AtomicReference<IPBanResult> matchRule = new AtomicReference<>();
+        boolean mr = fetchPeerFutures.stream().anyMatch(ele -> {
+            try {
+                IPBanResult ipBanResult = ele.get();
+                boolean match = ipBanResult.matchResult() == MatchResult.TRUE;
+                if (match) {
+                    matchRule.set(ipBanResult);
                 }
+                return match;
+            } catch (Exception e) {
+                log.error("IP黑名单订阅规则匹配异常", e);
+                return false;
             }
-        } catch (Exception e) {
-            log.error("IP黑名单订阅规则匹配异常", e);
-        }
+        });
         watch.stop();
         log.debug("匹配IP黑名单订阅规则花费时间：{}", watch.getLastTaskTimeNanos());
-        if (result != null) {
-            return result;
+        if (mr) {
+            return new BanResult(this, PeerAction.BAN, ip, String.format(Lang.MODULE_IBL_MATCH_IP_RULE, matchRule.get().ruleName()));
         }
         return new BanResult(this, PeerAction.NO_ACTION, "N/A", "No matches");
     }
@@ -112,7 +104,10 @@ public class IPBlackRuleList extends AbstractRuleFeatureModule {
 
     @Override
     public void onEnable() {
-        reloadConfig();
+        ConfigurationSection config = getConfig();
+        // 读取检查间隔
+        checkInterval = config.getLong("check-interval", checkInterval);
+        Executors.newScheduledThreadPool(1).scheduleAtFixedRate(this::reloadConfig, 0, checkInterval, java.util.concurrent.TimeUnit.MILLISECONDS);
     }
 
     /**
@@ -136,29 +131,37 @@ public class IPBlackRuleList extends AbstractRuleFeatureModule {
                 List<IPAddress> ipAddresses = new ArrayList<>();
                 if (null != url && url.startsWith("http")) {
                     // 解析远程订阅
-                    Digester digester = new Digester(DigestAlgorithm.SHA256);
                     String ruleFileName = ruleId + ".txt";
-                    File tempFile = new File(Main.getDataDirectory(), "temp_" + ruleId + ".txt");
-                    File ruleFile = new File(Main.getDataDirectory(), ruleFileName);
+                    File ruleDir = FileUtil.mkdir(new File(Main.getDataDirectory(), "/sub"));
+                    File tempFile = new File(ruleDir, "temp_" + ruleFileName);
+                    File ruleFile = new File(ruleDir, ruleFileName);
                     try {
-                        HTTPUtil.retryableSend(httpClient, MutableRequest.GET(url), HttpResponse.BodyHandlers.ofFile(Path.of(tempFile.getPath()))).whenComplete((pathHttpResponse, throwable) -> {
-                            if(!ruleFile.exists() || !digester.digestHex(tempFile).equals(digester.digestHex(ruleFile))){
+                        HTTPUtil.retryableSend(HTTPUtil.getHttpClient(false, null), MutableRequest.GET(url), HttpResponse.BodyHandlers.ofFile(Path.of(tempFile.getPath()))).whenComplete((pathHttpResponse, throwable) -> {
+                            if (throwable != null) {
+                                throw new RuntimeException(throwable);
+                            }
+                            String tempHash = Hashing.sha256().hashBytes(FileUtil.readBytes(tempFile)).toString();
+                            String ruleHash = null;
+                            if (ruleFile.exists()) {
+                                ruleHash = Hashing.sha256().hashBytes(FileUtil.readBytes(ruleFile)).toString();
+                            }
+                            if (!tempHash.equals(ruleHash)) {
                                 // 规则文件不存在或者规则文件与临时文件sha256不一致则需要更新
                                 fileToIPList(name, tempFile, ipAddresses);
                                 // 更新后重命名临时文件
                                 FileUtil.rename(tempFile, ruleFileName, true);
-                            }else{
+                            } else {
                                 // 如果一致，但bannedIps没有对应的规则内容，则加载内容
                                 if (bannedIps.stream().noneMatch(ele -> ele.getRuleId().equals(ruleId))) {
                                     fileToIPList(name, tempFile, ipAddresses);
-                                }else{
+                                } else {
                                     log.info("IP黑名单订阅规则 {} 未发生更新", name);
                                 }
                             }
                             FileUtil.del(tempFile);
                         }).join();
                         // ip列表不为空代表需要更新matcher
-                        if(!ipAddresses.isEmpty()){
+                        if (!ipAddresses.isEmpty()) {
                             // 如果已经存在则更新，否则添加
                             bannedIps.stream().filter(ele -> ele.getRuleId().equals(ruleId)).findFirst().ifPresentOrElse(ele -> {
                                 ele.setData(name, ipAddresses);
@@ -185,7 +188,6 @@ public class IPBlackRuleList extends AbstractRuleFeatureModule {
             }
             log.info("IP黑名单规则订阅完毕");
         }
-        lastCheckTime = System.currentTimeMillis();
     }
 
     /**
@@ -197,32 +199,19 @@ public class IPBlackRuleList extends AbstractRuleFeatureModule {
      */
     private void fileToIPList(String ruleName, File ruleFile, List<IPAddress> ipAddresses) {
         FileUtil.readLines(ruleFile, StandardCharsets.UTF_8).forEach(ele -> {
-            IPAddress ipAddress = new IPAddressString(ele).getAddress();
-            if (ipAddress != null) {
-                if (ipAddress.isIPv4Convertible()) {
-                    ipAddress = ipAddress.toIPv4();
-                }
-                ipAddresses.add(ipAddress);
-                log.debug("IP黑名单订阅规则 {} 加载IP : {}", ruleName, ele);
+            IPAddress ipAddress = IPAddressUtil.getIPAddress(ele);
+            if (ipAddress.isIPv4Convertible()) {
+                ipAddress = ipAddress.toIPv4();
             }
+            ipAddresses.add(ipAddress);
+            log.debug("IP黑名单订阅规则 {} 加载IP : {}", ruleName, ele);
         });
     }
 
     @Override
     public void onDisable() {
-
     }
+}
 
-    private void setupHttpClient() {
-        CookieManager cm = new CookieManager();
-        cm.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
-        this.httpClient = Methanol
-                .newBuilder()
-                .followRedirects(HttpClient.Redirect.ALWAYS)
-                .userAgent(Main.getUserAgent())
-                .defaultHeader("Content-Type", "application/octet-stream;charset=utf-8")
-                .requestTimeout(Duration.ofMinutes(1))
-                .connectTimeout(Duration.ofSeconds(10))
-                .cookieHandler(cm).build();
-    }
+record IPBanResult(String ruleName, MatchResult matchResult) {
 }
