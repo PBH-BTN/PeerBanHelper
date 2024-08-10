e5e978b2f665245e0c43281dd615fe96426292c1
Ghost_chu <ghostchu@111.com>
2024-08-10 23:40:28 +0800

    bump BTN-Protocol

--- src/main/java/com/ghostchu/peerbanhelper/Main.java
+++ src/main/java/com/ghostchu/peerbanhelper/Main.java
@@ -264,7 +264,7 @@ public class Main {
     }
 
     public static String getUserAgent() {
-        return "PeerBanHelper/" + meta.getVersion() + " BTN-Protocol/0.0.0-dev";
+        return "PeerBanHelper/" + meta.getVersion() + " BTN-Protocol/0.0.1";
     }
 
     public static boolean loadDependencies(String mavenManifestPath) throws IOException {
