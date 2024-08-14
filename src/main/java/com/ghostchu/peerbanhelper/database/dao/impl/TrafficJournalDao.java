fbca9c850ffd1de81b61a7ec7bfc3252662baa93
Ghost_chu <ghostchu@111.com>
2024-08-14 17:12:35 +0800

    校正初始时间戳

--- src/main/java/com/ghostchu/peerbanhelper/database/dao/impl/TrafficJournalDao.java
+++ src/main/java/com/ghostchu/peerbanhelper/database/dao/impl/TrafficJournalDao.java
@@ -3,6 +3,7 @@ package com.ghostchu.peerbanhelper.database.dao.impl;
 import com.ghostchu.peerbanhelper.database.Database;
 import com.ghostchu.peerbanhelper.database.dao.AbstractPBHDao;
 import com.ghostchu.peerbanhelper.database.table.TrafficJournalEntity;
+import com.ghostchu.peerbanhelper.util.MiscUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,7 +16,7 @@ public class TrafficJournalDao extends AbstractPBHDao<TrafficJournalEntity, Long
     }
 
     public TrafficJournalEntity getTodayJourney() throws SQLException {
-         return createIfNotExists(new TrafficJournalEntity(System.currentTimeMillis(), 0,0));
+         return createIfNotExists(new TrafficJournalEntity(MiscUtil.getStartOfToday(System.currentTimeMillis()), 0,0));
     }
 
 }
