b8e6b389c16e710251d66e423ebdaf0cdb29ddc1
Ghost_chu <ghostchu@111.com>
2024-08-21 17:31:38 +0800

    删除不再需要的 webui 设置脚本

--- setup-webui.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-current_wd=$(pwd)
-cd 'src/main/resources'
-rm -rf static
-git clone --depth 1  --branch gh-pages "https://github.com/PBH-BTN/pbh-fe.git" static
-cd static && echo "WebUI Version: $(git rev-parse --short HEAD)"
-cd $current_wd
