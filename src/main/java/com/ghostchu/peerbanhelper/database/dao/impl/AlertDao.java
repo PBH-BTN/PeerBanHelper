deb20fc658f85c77e66a69b73f3fa6a9be82c5ab
Ghost_chu <ghostchu@111.com>
2024-08-26 19:35:52 +0800

    initial alerts changes

---
+++ src/main/java/com/ghostchu/peerbanhelper/database/dao/impl/AlertDao.java
@@ -0,0 +1,24 @@
+package com.ghostchu.peerbanhelper.database.dao.impl;
+
+import com.ghostchu.peerbanhelper.database.Database;
+import com.ghostchu.peerbanhelper.database.dao.AbstractPBHDao;
+import com.ghostchu.peerbanhelper.database.table.AlertEntity;
+import com.ghostchu.peerbanhelper.util.paging.Page;
+import com.ghostchu.peerbanhelper.util.paging.Pageable;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.sql.SQLException;
+
+@Component
+@Slf4j
+public class AlertDao extends AbstractPBHDao<AlertEntity, Long> {
+    public AlertDao(@Autowired Database database) throws SQLException {
+        super(database.getDataSource(), AlertEntity.class);
+    }
+
+    public Page<AlertEntity> getUnreadAlerts(Pageable pageable) throws SQLException {
+        return queryByPaging(queryBuilder().orderBy("createAt", false).where().isNull("readAt").queryBuilder(), pageable);
+    }
+}
