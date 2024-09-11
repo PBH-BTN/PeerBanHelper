e1507841e84787a3c5bfd038f9905a50834c785e
Ghost_chu <ghostchu@111.com>
2024-09-12 00:44:07 +0800

    为 EcoQoS API 添加 Rollbar 错误上报

--- src/main/java/com/ghostchu/lib/jni/EcoMode.java
+++ src/main/java/com/ghostchu/lib/jni/EcoMode.java
@@ -1,7 +1,9 @@
 package com.ghostchu.lib.jni;
 
 import com.ghostchu.peerbanhelper.Main;
+import com.ghostchu.peerbanhelper.telemetry.rollbar.RollbarErrorReporter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
@@ -10,8 +12,15 @@ import java.nio.file.StandardCopyOption;
 import java.util.Locale;
 
 @Slf4j
+@Component
 public class EcoMode {
-    public static boolean ecoMode(boolean enable) {
+    private final RollbarErrorReporter rollbarErrorReporter;
+
+    public EcoMode(RollbarErrorReporter rollbarErrorReporter) {
+        this.rollbarErrorReporter = rollbarErrorReporter;
+    }
+
+    public boolean ecoMode(boolean enable) {
         String os = System.getProperty("os.name").toLowerCase(Locale.ROOT);
         if (!os.startsWith("win")) {
             throw new IllegalStateException("Only Windows OS support EcoMode API");
@@ -26,13 +35,15 @@ public class EcoMode {
                 Files.copy(Main.class.getResourceAsStream("/native/windows/ghost-common-jni_vc2015_amd64.dll"), tmpFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
             }
             System.load(tmpFile.getAbsolutePath());
-        } catch (IOException e) {
+        } catch (Exception e) {
             log.error("Unable load JNI native libraries", e);
+            rollbarErrorReporter.warning(e);
         }
         try {
             String data = setEcoMode(enable);
             return data.equals("SUCCESS");
         } catch (Throwable e) {
+            rollbarErrorReporter.warning(e);
             return false;
         }
     }
