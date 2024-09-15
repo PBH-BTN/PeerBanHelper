15af42437193caaa12ea194dbc46a9b368ae4eab
Ghost_chu <ghostchu@111.com>
2024-09-15 21:36:04 +0800

    感天动地 CI 跑通了

--- synopkg/build-script.sh
+++ synopkg/build-script.sh
@@ -13,8 +13,8 @@ mkdir -p /toolkit/source
 
 cp -av /tmp/pbh-packages/* /toolkit/source/
 sed -i "s#@IMAGE@#${IMAGE}#g" /toolkit/source/PeerBanHelperPackage/target/app/docker-compose.yaml
-cat /toolkit/source/PeerBanHelperPackage/target/app/docker-compose.yaml
+#cat /toolkit/source/PeerBanHelperPackage/target/app/docker-compose.yaml
 sed -i "s#@PBH_VERSION@#${PBH_VERSION}#g" /toolkit/source/PeerBanHelperPackage/INFO.sh
-cat /toolkit/source/PeerBanHelperPackage/INFO.sh
+#cat /toolkit/source/PeerBanHelperPackage/INFO.sh
 chmod +x /toolkit/source/PeerBanHelperPackage/INFO.sh
 /toolkit/pkgscripts-ng/PkgCreate.py -v 7.2 -p apollolake -c PeerBanHelperPackage
