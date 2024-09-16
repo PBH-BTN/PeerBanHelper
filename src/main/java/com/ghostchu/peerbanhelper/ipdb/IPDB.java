a314b16819626b7b4999e84b6eca52f527ac397f
Ghost_chu <ghostchu@111.com>
2024-09-16 14:18:11 +0800

    如果在给定时间内没有成功下载 IPDB，回落国内源

--- src/main/java/com/ghostchu/peerbanhelper/ipdb/IPDB.java
+++ src/main/java/com/ghostchu/peerbanhelper/ipdb/IPDB.java
@@ -280,7 +280,7 @@ public class IPDB implements AutoCloseable {
                 .defaultHeader("Accept-Encoding", "gzip,deflate")
                 .connectTimeout(Duration.of(15, ChronoUnit.SECONDS))
                 .headersTimeout(Duration.of(15, ChronoUnit.SECONDS))
-                .readTimeout(Duration.of(5, ChronoUnit.MINUTES))
+                .readTimeout(Duration.of(3, ChronoUnit.MINUTES))
                 .authenticator(new Authenticator() {
                     @Override
                     public PasswordAuthentication requestPasswordAuthenticationInstance(String host, InetAddress addr, int port, String protocol, String prompt, String scheme, URL url, RequestorType reqType) {
