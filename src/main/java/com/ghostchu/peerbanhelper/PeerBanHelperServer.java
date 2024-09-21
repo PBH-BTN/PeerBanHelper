6e4fbb7104fe9ac97315261cf8b474a032173b22
Ghost_chu <ghostchu@111.com>
2024-09-22 00:28:36 +0800

    添加配置文件 API

--- src/main/java/com/ghostchu/peerbanhelper/PeerBanHelperServer.java
+++ src/main/java/com/ghostchu/peerbanhelper/PeerBanHelperServer.java
@@ -654,6 +654,7 @@ public class PeerBanHelperServer implements Reloadable {
         moduleManager.register(PBHGeneralController.class);
         moduleManager.register(PBHTorrentController.class);
         moduleManager.register(PBHPeerController.class);
+        moduleManager.register(WebConfigController.class);
     }
 
     public Map<Downloader, Map<Torrent, List<Peer>>> collectPeers() {
