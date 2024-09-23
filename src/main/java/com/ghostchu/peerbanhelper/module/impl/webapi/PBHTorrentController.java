f45bc54b589f05acb3bfa675d61e660f5f4371d4
Ghost_chu <ghostchu@111.com>
2024-09-23 13:10:38 +0800

    支持按 name/hash 检索、支持 banHistory 检索

--- src/main/java/com/ghostchu/peerbanhelper/module/impl/webapi/PBHTorrentController.java
+++ src/main/java/com/ghostchu/peerbanhelper/module/impl/webapi/PBHTorrentController.java
@@ -3,6 +3,7 @@ package com.ghostchu.peerbanhelper.module.impl.webapi;
 import com.ghostchu.peerbanhelper.database.dao.impl.HistoryDao;
 import com.ghostchu.peerbanhelper.database.dao.impl.PeerRecordDao;
 import com.ghostchu.peerbanhelper.database.dao.impl.TorrentDao;
+import com.ghostchu.peerbanhelper.database.table.HistoryEntity;
 import com.ghostchu.peerbanhelper.database.table.PeerRecordEntity;
 import com.ghostchu.peerbanhelper.module.AbstractFeatureModule;
 import com.ghostchu.peerbanhelper.text.Lang;
@@ -57,7 +58,27 @@ public class PBHTorrentController extends AbstractFeatureModule {
                 .get("/api/torrent", this::handleTorrentList, Role.USER_READ)
                 .get("/api/torrent/search", this::handleTorrentSearch, Role.USER_READ)
                 .get("/api/torrent/{infoHash}", this::handleTorrentInfo, Role.USER_READ)
-                .get("/api/torrent/{infoHash}/accessHistory", this::handleConnectHistory, Role.USER_READ);
+                .get("/api/torrent/{infoHash}/accessHistory", this::handleConnectHistory, Role.USER_READ)
+                .get("/api/torrent/{infoHash}/banHistory", this::handleBanHistory, Role.USER_READ);
+    }
+
+    private void handleBanHistory(Context ctx) throws SQLException {
+        var torrent = torrentDao.queryByInfoHash(ctx.pathParam("infoHash"));
+        if (torrent.isEmpty()) {
+            ctx.status(404);
+            ctx.json(new StdResp(false, tl(locale(ctx), Lang.TORRENT_NOT_FOUND), null));
+            return;
+        }
+        Pageable pageable = new Pageable(ctx);
+        var t = torrent.get();
+        Page<HistoryEntity> page = historyDao.queryByPaging(
+                historyDao.queryBuilder()
+                        .orderBy("banAt", false)
+                        .where()
+                        .eq("torrent_id", t)
+                        .queryBuilder()
+                , pageable);
+        ctx.json(new StdResp(true, null, page));
     }
 
     private void handleTorrentSearch(Context ctx) throws SQLException {
@@ -70,7 +91,19 @@ public class PBHTorrentController extends AbstractFeatureModule {
 
     private void handleTorrentList(Context ctx) throws SQLException {
         Pageable pageable = new Pageable(ctx);
-        ctx.json(new StdResp(true, null, torrentDao.queryByPaging(pageable)));
+        if (ctx.queryParam("keyword") == null) {
+            ctx.json(new StdResp(true, null, torrentDao.queryByPaging(pageable)));
+        } else {
+            ctx.json(new StdResp(true, null, torrentDao.queryByPaging(
+                    torrentDao
+                            .queryBuilder()
+                            .where()
+                            .like("name", ctx.queryParam("keyword"))
+                            .or()
+                            .like("hash", ctx.queryParam("keyword"))
+                            .queryBuilder()
+                    , pageable)));
+        }
     }
 
 
@@ -113,6 +146,11 @@ public class PBHTorrentController extends AbstractFeatureModule {
         ctx.json(new StdResp(true, null, page));
     }
 
+    @Override
+    public void onDisable() {
+
+    }
+
     public record TorrentInfo(
             String infoHash,
             String name,
@@ -120,11 +158,6 @@ public class PBHTorrentController extends AbstractFeatureModule {
             long peerBanCount,
             long peerAccessCount
     ) {
-
-    }
-
-    @Override
-    public void onDisable() {
 
     }
 }
