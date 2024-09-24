2da8cbf1d7e9ee8578dda5bcb6e0f8a8651e2667
Ghost_chu <30802565+Ghost-chu@users.noreply.github.com>
2024-09-25 05:15:42 +0800

    New translations en-us.ts (English, United States)

--- crowdin-webui/webui/src/views/rule-management/locale/en-US.ts
+++ crowdin-webui/webui/src/views/rule-management/locale/en-US.ts
@@ -1,4 +1,4 @@
-import subscription from '../components/subscribe/locale/en-US';
+import subscription from "../components/generic/locale/en-US";
 import generic from '../components/generic/locale/en-US';
 export default {
   ...subscription,
