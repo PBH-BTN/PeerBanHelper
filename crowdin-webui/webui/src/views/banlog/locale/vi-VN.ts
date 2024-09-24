ed2739a08f54c0d45425f35ee094beeab7d59707
Ghost_chu <30802565+Ghost-chu@users.noreply.github.com>
2024-09-25 02:46:39 +0800

    New translations zh-cn.ts (Vietnamese)

---
+++ crowdin-webui/webui/src/views/banlog/locale/vi-VN.ts
@@ -0,0 +1,14 @@
+export default {
+  'page.banlog.description': ' 此页面展示 PeerBanHelper 储存的封禁历史记录信息。',
+  'page.banlog.banlogTable.notUnbanned': '未解封',
+  'page.banlog.banlogTable.column.banTime': '封禁时间',
+  'page.banlog.banlogTable.column.unbanTime': '解封时间',
+  'page.banlog.banlogTable.column.peerAddress': 'Peer 地址',
+  'page.banlog.banlogTable.column.peerId': 'Peer ID',
+  'page.banlog.banlogTable.column.trafficSnapshot': '流量快照',
+  'page.banlog.banlogTable.column.torrentName': '种子名',
+  'page.banlog.banlogTable.column.torrentSize': '大小',
+  'page.banlog.banlogTable.column.description': '描述',
+  'page.banlog.radio.viewMode': '视图模式',
+  'page.banlog.radio.table': '表格'
+};
