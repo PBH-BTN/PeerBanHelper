d50c842a28bff70b7dd90f76260e3b939d22bc70
Ghost_chu <30802565+Ghost-chu@users.noreply.github.com>
2024-09-25 02:45:26 +0800

    New translations zh-cn.ts (Chinese Traditional)

---
+++ crowdin-webui/webui/src/service/locale/zh-TW.ts
@@ -0,0 +1,5 @@
+export default {
+  'service.manifest.networkError': '网络故障，无法发起请求',
+  'service.manifest.parseError': '无法解析接口返回数据，请检查服务地址是否正确',
+  'service.manifest.formatError': '后端接口响应格式错误，请检查后端版本'
+};
