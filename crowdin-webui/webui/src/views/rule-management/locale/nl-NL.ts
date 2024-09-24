869db08d434eec01155c34f421d38fd6f3f3f426
Ghost_chu <30802565+Ghost-chu@users.noreply.github.com>
2024-09-25 03:03:26 +0800

    New translations en-us.ts (Dutch)

--- crowdin-webui/webui/src/views/rule-management/locale/nl-NL.ts
+++ crowdin-webui/webui/src/views/rule-management/locale/nl-NL.ts
@@ -1,5 +1,5 @@
-import subscription from '../components/subscribe/locale/zh-CN';
-import generic from '../components/generic/locale/zh-CN';
+import subscription from '../components/subscribe/locale/en-US';
+import generic from '../components/generic/locale/en-US';
 export default {
   ...subscription,
   ...generic
