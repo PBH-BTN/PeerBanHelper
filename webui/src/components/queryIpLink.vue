cf8eb93768a7ec39cd08575c31af6af2b5d8a5f4
Gaojianli <admin@gaojianli.me>
2024-09-25 19:34:48 +0800

    fix lint

--- webui/src/components/queryIpLink.vue
+++ webui/src/components/queryIpLink.vue
@@ -1,14 +1,10 @@
 <template>
-  <a
-    style="text-decoration: none; cursor: pointer"
-    :style="style"
-    :href="`ipHistory?ip=${ip}`"
-  >
+  <a style="text-decoration: none; cursor: pointer" :style="style" :href="`ipHistory?ip=${ip}`">
     <slot></slot>
   </a>
 </template>
 <script lang="ts" setup>
-import type { StyleValue } from 'vue';
+import type { StyleValue } from 'vue'
 
 const { ip } = defineProps<{
   ip: string
