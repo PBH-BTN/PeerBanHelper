ebefbf9e32aca6f4cb27a2d448473c1a5cda982c
Ghost_chu <ghostchu@111.com>
2024-10-18 01:55:30 +0800

    新增 BTN Peers 活动历史记录协议
    提升 BTN 版本号

---
+++ src/main/java/com/ghostchu/peerbanhelper/btn/ability/BtnAbilitySubmitHistory.java
@@ -0,0 +1,99 @@
+package com.ghostchu.peerbanhelper.btn.ability;
+
+import com.ghostchu.peerbanhelper.btn.BtnNetwork;
+import com.ghostchu.peerbanhelper.btn.ping.BtnPeerHistory;
+import com.ghostchu.peerbanhelper.btn.ping.BtnPeerHistoryPing;
+import com.ghostchu.peerbanhelper.text.Lang;
+import com.ghostchu.peerbanhelper.util.HTTPUtil;
+import com.ghostchu.peerbanhelper.util.json.JsonUtil;
+import com.ghostchu.peerbanhelper.util.paging.Pageable;
+import com.github.mizosoft.methanol.MutableRequest;
+import com.google.gson.JsonObject;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import static com.ghostchu.peerbanhelper.text.TextManager.tlUI;
+
+@Slf4j
+public class BtnAbilitySubmitHistory extends AbstractBtnAbility {
+    private final BtnNetwork btnNetwork;
+    private final long interval;
+    private final String endpoint;
+    private final long randomInitialDelay;
+    private long lastSubmitAt = System.currentTimeMillis();
+
+    public BtnAbilitySubmitHistory(BtnNetwork btnNetwork, JsonObject ability) {
+        this.btnNetwork = btnNetwork;
+        this.interval = ability.get("interval").getAsLong();
+        this.endpoint = ability.get("endpoint").getAsString();
+        this.randomInitialDelay = ability.get("random_initial_delay").getAsLong();
+    }
+
+
+    @Override
+    public void load() {
+        setLastStatus(true, "No content reported to remote yet");
+        btnNetwork.getExecuteService().scheduleWithFixedDelay(this::submit, interval + new Random().nextLong(randomInitialDelay), interval, TimeUnit.MILLISECONDS);
+    }
+
+    private void submit() {
+        try {
+            log.info(tlUI(Lang.BTN_SUBMITTING_PEERS));
+            List<BtnPeerHistory> btnPeers = generatePing();
+            if (btnPeers.isEmpty()) {
+                setLastStatus(true, "Last report is empty, skipped.");
+                return;
+            }
+            BtnPeerHistoryPing ping = new BtnPeerHistoryPing(
+                    System.currentTimeMillis(),
+                    btnPeers
+            );
+            MutableRequest request = MutableRequest.POST(endpoint
+                    , HTTPUtil.gzipBody(JsonUtil.getGson().toJson(ping).getBytes(StandardCharsets.UTF_8))
+            ).header("Content-Encoding", "gzip");
+            HTTPUtil.nonRetryableSend(btnNetwork.getHttpClient(), request, HttpResponse.BodyHandlers.ofString())
+                    .thenAccept(r -> {
+                        if (r.statusCode() != 200) {
+                            log.error(tlUI(Lang.BTN_REQUEST_FAILS, r.statusCode() + " - " + r.body()));
+                            setLastStatus(false, "HTTP Error: " + r.statusCode() + " - " + r.body());
+                        } else {
+                            log.info(tlUI(Lang.BTN_SUBMITTED_PEERS, btnPeers.size()));
+                            setLastStatus(true, "Reported " + btnPeers.size() + " entries.");
+                            lastSubmitAt = System.currentTimeMillis();
+                        }
+                    })
+                    .exceptionally(e -> {
+                        log.warn(tlUI(Lang.BTN_REQUEST_FAILS), e);
+                        setLastStatus(false, e.getClass().getName() + ": " + e.getMessage());
+                        return null;
+                    });
+        } catch (Throwable e) {
+            log.error("Unable to submit peers", e);
+            setLastStatus(false, "Unknown Error: " + e.getClass().getName() + ": " + e.getMessage());
+        }
+    }
+
+
+    @SneakyThrows
+    private List<BtnPeerHistory> generatePing() {
+        Pageable pageable = new Pageable(0, 100000); // 再多的话，担心爆内存
+        return btnNetwork.getPeerRecordDao().getPendingSubmitPeerRecords(pageable,
+                        new Timestamp(lastSubmitAt)).getResults().stream()
+                .map(BtnPeerHistory::from).collect(Collectors.toList());
+    }
+
+    @Override
+    public void unload() {
+
+    }
+
+
+}
---
+++ src/main/java/com/ghostchu/peerbanhelper/btn/ping/BtnPeerHistoryPing.java
@@ -0,0 +1,19 @@
+package com.ghostchu.peerbanhelper.btn.ping;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BtnPeerHistoryPing {
+    @SerializedName("populate_time")
+    private long populateTime;
+    @SerializedName("peers")
+    private List<BtnPeerHistory> peers;
+
+}
