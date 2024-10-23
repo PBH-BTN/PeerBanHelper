6cd890766d4b925fbc182538f4ab2d699fcd5ab6
paulzzh <28055560+paulzzh@users.noreply.github.com>
2024-10-24 02:10:27 +0800

    windows 返回 build 号

--- src/main/java/com/ghostchu/peerbanhelper/module/impl/webapi/PBHGeneralController.java
+++ src/main/java/com/ghostchu/peerbanhelper/module/impl/webapi/PBHGeneralController.java
@@ -159,7 +159,11 @@ public class PBHGeneralController extends AbstractFeatureModule {
         var operatingSystem = systemInfo.getOperatingSystem();
         Map<String, Object> os = new LinkedHashMap<>();
         os.put("os", osMXBean.getName());
-        os.put("version", osMXBean.getVersion());
+        if (osMXBean.getName().contains("Windows")) {
+            os.put("version", String.valueOf(operatingSystem));
+        } else {
+            os.put("version", osMXBean.getVersion());
+        }
         os.put("architecture", osMXBean.getArch());
         os.put("cores", osMXBean.getAvailableProcessors());
         //os.put("family", operatingSystem.getFamily());
