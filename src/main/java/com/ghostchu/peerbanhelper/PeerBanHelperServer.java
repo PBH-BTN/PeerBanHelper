6cad59855b995be647b484b4db6b6e2e0ff11220
Ghost_chu <ghostchu@111.com>
2024-10-25 16:33:04 +0800

    降级封禁 log 的等级为 INFO

--- src/main/java/com/ghostchu/peerbanhelper/PeerBanHelperServer.java
+++ src/main/java/com/ghostchu/peerbanhelper/PeerBanHelperServer.java
@@ -518,7 +518,7 @@ public class PeerBanHelperServer implements Reloadable {
                                     relaunch.add(detail.torrent());
                                     banPeer(banlistClone, banMetadata, detail.torrent(), detail.peer());
                                     if (detail.result().action() != PeerAction.BAN_FOR_DISCONNECT) {
-                                        log.warn(tlUI(Lang.BAN_PEER, detail.peer().getPeerAddress(), detail.peer().getPeerId(), detail.peer().getClientName(), detail.peer().getProgress(), detail.peer().getUploaded(), detail.peer().getDownloaded(), detail.torrent().getName(), tl(DEF_LOCALE, detail.result().reason())));
+                                        log.info(tlUI(Lang.BAN_PEER, detail.peer().getPeerAddress(), detail.peer().getPeerId(), detail.peer().getClientName(), detail.peer().getProgress(), detail.peer().getUploaded(), detail.peer().getDownloaded(), detail.torrent().getName(), tl(DEF_LOCALE, detail.result().reason())));
                                     }
                                 }
                             });
