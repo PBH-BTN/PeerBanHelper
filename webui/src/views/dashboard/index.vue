16fdd3233ee1f2a401d2d6f8c4f6d50141fa4cd1
Gaojianli <823805669hehe@gmail.com>
2024-10-27 00:19:55 +0800

    dashboard 加跳转

--- webui/src/views/dashboard/index.vue
+++ webui/src/views/dashboard/index.vue
@@ -2,15 +2,22 @@
 import ClientStatus from './components/clientStatus.vue'
 import StatisticInfo from './components/statisticInfo.vue'
 import { useI18n } from 'vue-i18n'
+import { useViewRoute } from '@/router'
 
 const { t } = useI18n()
+const [_r, _c, goto] = useViewRoute()
 </script>
 
 <template>
   <a-space direction="vertical" fill>
-    <a-typography-text style="font-size: 1.2em">
-      {{ t('page.dashboard.description') }}
-    </a-typography-text>
+    <a-space style="display: flex; justify-content: space-between">
+      <a-typography-text style="font-size: 1.2em">
+        {{ t('page.dashboard.description') }}
+      </a-typography-text>
+      <a-button type="text" @click="goto('config')">{{
+        t('page.dashboard.runningStatus')
+      }}</a-button>
+    </a-space>
     <StatisticInfo />
     <br />
     <ClientStatus />
