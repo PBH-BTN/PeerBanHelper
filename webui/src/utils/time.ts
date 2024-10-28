b2fdff19099cb34a220d6fe7cdf0a9f8b9de77e9
Gaojianli <admin@gaojianli.me>
2024-10-28 20:13:28 +0800

    use intl.durationFormat

--- webui/src/utils/time.ts
+++ webui/src/utils/time.ts
@@ -1,12 +1,58 @@
-import dayjs from 'dayjs'
-import Duration from 'dayjs/plugin/duration'
-import RelativeTime from 'dayjs/plugin/relativeTime'
-dayjs.extend(RelativeTime)
-dayjs.extend(Duration)
+import { DurationFormat } from '@formatjs/intl-durationformat'
+
+function parseTimeDuration(ms: number) {
+  const nanoseconds = (ms * 1e6) % 1e3
+  const microseconds = Math.floor(ms * 1e3) % 1e3
+  const milliseconds = Math.floor(ms) % 1e3
+
+  const totalSeconds = Math.floor(ms / 1000)
+  const seconds = totalSeconds % 60
+
+  const totalMinutes = Math.floor(totalSeconds / 60)
+  const minutes = totalMinutes % 60
+
+  const totalHours = Math.floor(totalMinutes / 60)
+  const hours = totalHours % 24
+
+  const totalDays = Math.floor(totalHours / 24)
+  const days = totalDays % 7
+
+  const totalWeeks = Math.floor(totalDays / 7)
+  const weeks = totalWeeks % 4
+
+  const totalMonths = Math.floor(totalWeeks / 4)
+  const months = totalMonths % 12
+
+  const years = Math.floor(totalMonths / 12)
+
+  return {
+    years,
+    months,
+    weeks,
+    days,
+    hours,
+    minutes,
+    seconds,
+    milliseconds,
+    microseconds,
+    nanoseconds
+  }
+}
+function getFormatter(): DurationFormat {
+  // @ts-expect-error Supported on chrome 129
+  if (Intl.DurationFormat !== undefined) {
+    // @ts-expect-error Supported on chrome 129
+    return new Intl.DurationFormat()
+  } else {
+    return new DurationFormat()
+  }
+}
+
 export function formatMilliseconds(ms: number): string {
-  return dayjs.duration({ milliseconds: ms }).humanize()
+  const formatter = getFormatter()
+  return formatter.format(parseTimeDuration(ms))
 }
 
 export function formatSeconds(seconds: number): string {
-  return dayjs.duration({ seconds: seconds }).humanize()
+  return formatMilliseconds(seconds * 1000)
 }
