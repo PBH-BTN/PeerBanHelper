07051c84900b6d529d202e08f5eb8ea14b723df0
Gaojianli <Gaojianli@users.noreply.github.com>
2024-10-30 15:24:24 +0000

    deploy: e9c872c0b059a828c5b2bbfac8b727d9b858d164

---
+++ assets/queryIpLink.vue_vue_type_script_setup_true_lang-HZkvcNLr.js
@@ -0,0 +1,1 @@
+import{f as o,j as r,k as t,l as n,m as s}from"./libs-D-uoNQCp.js";const a=["href"],c=o({__name:"queryIpLink",props:{ip:{},style:{type:[Boolean,null,String,Object,Array]}},setup(p){return(e,l)=>(r(),t("a",{style:s([{"text-decoration":"none",cursor:"pointer"},e.style]),href:`ipHistory?ip=${e.ip}`},[n(e.$slots,"default")],12,a))}});export{c as _};
