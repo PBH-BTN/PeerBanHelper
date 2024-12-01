7707cc891751bd81a4c0000a012fd8364483edb3
Ghost_chu <ghostchu@111.com>
2024-12-01 12:47:28 +0800

    允许配置文件控制 IPFS 启用

--- src/main/java/com/ghostchu/peerbanhelper/decentralized/DecentralizedManager.java
+++ src/main/java/com/ghostchu/peerbanhelper/decentralized/DecentralizedManager.java
@@ -1,6 +1,9 @@
 package com.ghostchu.peerbanhelper.decentralized;
 
+import com.ghostchu.peerbanhelper.Main;
 import com.ghostchu.peerbanhelper.text.Lang;
+import com.ghostchu.simplereloadlib.ReloadResult;
+import com.ghostchu.simplereloadlib.Reloadable;
 import io.ipfs.api.IPFS;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
@@ -11,19 +14,30 @@ import static com.ghostchu.peerbanhelper.text.TextManager.tlUI;
 
 @Component
 @Slf4j
-public class DecentralizedManager implements AutoCloseable{
+public class DecentralizedManager implements AutoCloseable, Reloadable {
     @Getter
     @Nullable
     private IPFS ipfs;
+
     public DecentralizedManager() {
         startupIPFS();
     }
 
+    @Override
+    public ReloadResult reloadModule() throws Exception {
+        startupIPFS();
+        return Reloadable.super.reloadModule();
+    }
+
     private void startupIPFS() {
+        if (Main.getMainConfig().getBoolean("decentralized.enabled", false)) {
+            this.ipfs = null;
+            return;
+        }
         try {
-            this.ipfs = new IPFS("/ip4/127.0.0.1/tcp/5001");
+            this.ipfs = new IPFS(Main.getMainConfig().getString("decentralized.kubo-rpc", "/ip4/127.0.0.1/tcp/5001"));
             log.info(tlUI(Lang.IPFS_INIT_WELCOME, ipfs.version()));
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error(tlUI(Lang.IPFS_INIT_FAILED), e);
         }
     }
