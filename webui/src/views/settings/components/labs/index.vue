f1d2b0e63f62a8b921e0798c3bdce2a5766eac97
Gaojianli <admin@gaojianli.me>
2024-12-06 15:10:54 +0800

    finish labs

---
+++ webui/src/views/settings/components/labs/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <a-space direction="vertical" fill>
+    <a-typography-title :heading="3">
+      {{ t('page.settings.tab.labs') }}
+    </a-typography-title>
+    <a-alert :title="t('page.settings.tab.labs.welcome')">
+      <i18n-t keypath="page.settings.tab.labs.welcome.content">
+        <template #github>
+          <a-link href="https://github.com/PBH-BTN/PeerBanHelper/issues">Github</a-link>
+        </template>
+      </i18n-t>
+    </a-alert>
+    <a-typography-title :heading="5">
+      {{ t('page.settings.tab.labs.list') }}
+    </a-typography-title>
+    <div style="width: 100%; display: flex; justify-content: center">
+      <a-list :data="data?.data.experiments" style="max-width: 60rem" :loading="loading">
+        <template #item="{ item }">
+          <a-list-item action-layout="vertical">
+            <template #actions>
+              <a-space
+                >{{ t('page.settings.tab.labs.action.enable') }}
+                <a-switch
+                  v-model="item.activated"
+                  size="small"
+                  :before-change="(value) => switchExperimentStatus(item.id, value as boolean)"
+                />
+              </a-space>
+            </template>
+            <a-list-item-meta>
+              <template #title>
+                {{ item.title }} &nbsp;
+                <a-tag v-if="item.activated" color="green">{{
+                  t('page.settings.tab.labs.enabled')
+                }}</a-tag>
+              </template>
+              <template #description>
+                <!--eslint-disable-next-line vue/no-v-html-->
+                <div v-html="md.render(item.description)"></div>
+              </template>
+            </a-list-item-meta>
+          </a-list-item>
+        </template>
+      </a-list>
+    </div>
+  </a-space>
+</template>
+<script setup lang="ts">
+import { GetExperimentList, SetExperimentStatus } from '@/service/labs'
+import { Message } from '@arco-design/web-vue'
+import markdownit from 'markdown-it'
+import { useI18n } from 'vue-i18n'
+import { useRequest } from 'vue-request'
+const { t } = useI18n()
+const { data, loading, refresh } = useRequest(GetExperimentList)
+const md = new markdownit()
+const switchExperimentStatus = async (id: string, activated: boolean) => {
+  try {
+    const res = await SetExperimentStatus(id, activated)
+    if (res.success) {
+      refresh()
+      return true
+    } else throw new Error(res.message)
+  } catch (e) {
+    if (e instanceof Error) Message.error(e.message)
+    return false
+  }
+}
+</script>
---
+++ webui/src/views/settings/components/labs/locale/zh-CN.ts
@@ -0,0 +1,9 @@
+export default {
+  'page.settings.tab.labs': '实验室',
+  'page.settings.tab.labs.welcome': '欢迎来到实验室',
+  'page.settings.tab.labs.welcome.content':
+    '这里是实验室，我们会在这里尝试一些不够稳定的新功能，这些功能对于封禁效果可能会更好或者更差，请根据你的需求自行开关。如果您有任何建议或者想法，欢迎在 {github} 上提交 issue 或者 PR。',
+  'page.settings.tab.labs.list': '实验列表',
+  'page.settings.tab.labs.action.enable': '启用',
+  'page.settings.tab.labs.enabled': '已启用'
+}
