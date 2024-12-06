9d6b668097efbf7caaa67914d3792e5730a8decd
Ghost_chu <ghostchu@111.com>
2024-12-06 20:14:40 +0800

    更改文本

--- webui/src/views/settings/components/labs/locale/en-US.ts
+++ webui/src/views/settings/components/labs/locale/en-US.ts
@@ -2,7 +2,7 @@ export default {
   'page.settings.tab.labs': 'Labs',
   'page.settings.tab.labs.welcome': 'Welcome to Labs',
   'page.settings.tab.labs.welcome.content':
-    'This is the Lab, where we will try some unstable new features. These features may have better or worse effects on the ban. Please turn them on or off according to your needs. If you have any suggestions or ideas, please submit an issue or PR on {github}.',
+    'We occasionally make experimental changes to some features of PeerBanHelper. These experiments are ongoing; some might be discontinued. We hope most of them will receive your feedback, be improved, and eventually become part of PeerBanHelper. This is the purpose of the PeerBanHelper Lab.\nIf you have any suggestions or ideas, feel free to submit an issue or a pull request on {github}. Each experiment is randomly enabled for a subset of PeerBanHelper users upon release. You can always use the toggle below to choose whether to disable or enable these experiments.',
   'page.settings.tab.labs.list': 'Experiment List',
   'page.settings.tab.labs.action.enable': 'Enable',
   'page.settings.tab.labs.enabled': 'Enabled'
--- webui/src/views/settings/components/labs/locale/zh-CN.ts
+++ webui/src/views/settings/components/labs/locale/zh-CN.ts
@@ -2,7 +2,7 @@ export default {
   'page.settings.tab.labs': '实验室',
   'page.settings.tab.labs.welcome': '欢迎来到实验室',
   'page.settings.tab.labs.welcome.content':
-    '这里是实验室，我们会在这里尝试一些不够稳定的新功能，这些功能对于封禁效果可能会更好或者更差，请根据你的需求自行开关。如果您有任何建议或者想法，欢迎在 {github} 上提交 issue 或者 PR。',
+    '我们有时会对 PeerBanHelper 的一些功能做出实验性的更改。这些实验正在进行中，有的则可能被放弃。我们希望其中的多数会获得您的反馈意见，得以改进，并最终成为 PeerBanHelper 的一部分。PeerBanHelper 实验室的作用就在于此。\n如果您有任何建议或者想法，欢迎在 {github} 上提交 Issue 或者 PR。每个实验推出时都会随机的在一部分用户的 PeerBanHelper 上启用，您随时可以通过下面的开关决定禁用还是启用这些实验。',
   'page.settings.tab.labs.list': '实验列表',
   'page.settings.tab.labs.action.enable': '启用',
   'page.settings.tab.labs.enabled': '已启用'
