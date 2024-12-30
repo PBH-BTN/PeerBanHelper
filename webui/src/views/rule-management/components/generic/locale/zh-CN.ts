21af55c69da080c087dee063fe4a4a502b7885af
CreeperAWA <CreeperAWA@creeper.us.kg>
2024-12-30 17:00:17 +0800

    基于{type}的规则

--- webui/src/views/rule-management/components/generic/locale/zh-CN.ts
+++ webui/src/views/rule-management/components/generic/locale/zh-CN.ts
@@ -5,7 +5,7 @@ export default {
   'page.rule_management.region': '地区',
   'page.rule_management.netType': '网络类型',
   'page.rule_management.city': '城市',
-  'page.rule_management.generic.description': '您可以在这里管理基于 {type} 的规则。',
+  'page.rule_management.generic.description': '您可以在这里管理基于{type}的规则。',
   'page.rule_management.generic.addOne': '新增',
   'page.rule_management.ip.placeholder': '192.168.1.0/24',
   'page.rule_management.asn.placeholder': '114514',
