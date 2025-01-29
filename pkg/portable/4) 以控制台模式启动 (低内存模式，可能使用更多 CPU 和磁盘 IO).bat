192422731bfacc671239cfe01f9fed7b3e5a2114
Ghost_chu <ghostchu@111.com>
2025-01-29 21:00:58 +0800

    添加低内存模式启动变体

---
+++ pkg/portable/4) 以控制台模式启动 (低内存模式，可能使用更多 CPU 和磁盘 IO).bat
@@ -0,0 +1,5 @@
+@echo off
+chcp 65001
+title PeerBanHelper (低内存模式，控制台，将使用更多 CPU 和磁盘 I/O)
+start ./jre/bin/java.exe -Xmx128M -Xms16M -Xss512k -XX:+UseG1GC -XX:+UseStringDeduplication -XX:+ShrinkHeapInSteps -Dfile.encoding=UTF-8 -Dpbh.usePlatformConfigLocation=true -Dpbh.release=install4j -Djava.awt.headless=true -Dpbh.geoIpCache.timeout=30000 -Dpbh.geoIpCache.size=100 -Dpbh.moduleMatchCache.weight=25000 -Dpbh.modulematchcache.timeout=30000 -Dpbh.gui.logs.maxSize=50 -Dpbh.logger.logEntryDeque.size=100 -Dpbh.logger.ringDeque.size=50 -Dpbh.module.activeMonitorModule.diskWriteCache.timeout=30000 -Dpbh.module.activeMonitorModule.diskWriteCache.size=1500 -Dpbh.module.progressCheatBlocker.recorder.timeout=30000 -Dpbh.module.progressCheatBlocker.recorder.weight=6000000 -jar PeerBanHelper.jar nogui nojcef
+pause
