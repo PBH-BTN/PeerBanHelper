428452d619e090fee5f26d6198195e6d09606ce0
Ghost_chu <ghostchu@111.com>
2025-01-30 02:02:51 +0800

    默认对所有参与实验室的用户启用 SQLITE_VACUUM 和 DNSJAVA 实验

--- src/main/java/com/ghostchu/peerbanhelper/lab/Experiments.java
+++ src/main/java/com/ghostchu/peerbanhelper/lab/Experiments.java
@@ -7,9 +7,9 @@ import java.util.Collections;
 import java.util.List;
 
 public enum Experiments {
-    IPFS(new Experiment("ipfs", List.of(), new TranslationComponent(Lang.LAB_EXPERIMENT_IPFS_TITLE), new TranslationComponent(Lang.LAB_EXPERIMENT_IPFS_DESCRIPTION))),
-    DNSJAVA(new Experiment("dnsjava", List.of(0, 1, 2), new TranslationComponent(Lang.LAB_EXPERIMENT_DNSJAVA_TITLE), new TranslationComponent(Lang.LAB_EXPERIMENT_DNSJAVA_DESCRIPTION))),
-    SQLITE_VACUUM(new Experiment("sqlite_vacuum", List.of(0, 1, 3, 5), new TranslationComponent(Lang.LAB_EXPERIMENT_SQLITE_VACUUM_TITLE), new TranslationComponent(Lang.LAB_EXPERIMENT_SQLITE_VACUUM_DESCRIPTION))),
+    IPFS(new Experiment("ipfs", Collections.emptyList(), new TranslationComponent(Lang.LAB_EXPERIMENT_IPFS_TITLE), new TranslationComponent(Lang.LAB_EXPERIMENT_IPFS_DESCRIPTION))),
+    DNSJAVA(new Experiment("dnsjava", List.of(0, 1, 2, 3, 4), new TranslationComponent(Lang.LAB_EXPERIMENT_DNSJAVA_TITLE), new TranslationComponent(Lang.LAB_EXPERIMENT_DNSJAVA_DESCRIPTION))),
+    SQLITE_VACUUM(new Experiment("sqlite_vacuum", List.of(0, 1, 2, 3, 4), new TranslationComponent(Lang.LAB_EXPERIMENT_SQLITE_VACUUM_TITLE), new TranslationComponent(Lang.LAB_EXPERIMENT_SQLITE_VACUUM_DESCRIPTION))),
     FILL_MISSING_DATA_IN_TRAFFIC_SUMMARY(new Experiment("fill_missing_data_in_traffic_summary", Collections.emptyList(), new TranslationComponent(Lang.LAB_EXPERIMENT_FILL_MISSING_DATA_IN_TRAFFIC_SUMMARY_TITLE), new TranslationComponent(Lang.LAB_EXPERIMENT_FILL_MISSING_DATA_IN_TRAFFIC_SUMMARY_DESCRIPTION))),
     TRANSACTION_BATCH_BAN_HISTORY_WRITE(new Experiment("transaction_batch_ban_history_write", List.of(2, 5), new TranslationComponent(Lang.LAB_EXPERIMENT_TRANSACTION_BATCH_BAN_HISTORY_WRITE_TITLE), new TranslationComponent(Lang.LAB_EXPERIMENT_TRANSACTION_BATCH_BAN_HISTORY_WRITE_DESCRIPTION))),
     ;
