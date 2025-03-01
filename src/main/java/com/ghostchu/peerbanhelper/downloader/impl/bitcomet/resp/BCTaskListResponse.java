bf040f7c72911f5bb9b668b6122650d4f1b97d37
Ghost_chu <ghostchu@111.com>
2025-03-01 16:07:40 +0800

    尽量使用 primitive 类型压缩内存使用

--- src/main/java/com/ghostchu/peerbanhelper/downloader/impl/bitcomet/resp/BCTaskListResponse.java
+++ src/main/java/com/ghostchu/peerbanhelper/downloader/impl/bitcomet/resp/BCTaskListResponse.java
@@ -12,8 +12,6 @@ public final class BCTaskListResponse {
 
     @SerializedName("tasks")
     private List<TasksDTO> tasks;
-//    @SerializedName("version")
-//    private String version;
 
     @NoArgsConstructor
     @Data
@@ -22,27 +20,5 @@ public final class BCTaskListResponse {
         private long taskId;
         @SerializedName("type")
         private String type;
-//        @SerializedName("task_name")
-//        private String taskName;
-//        @SerializedName("status")
-//        private String status;
-//        @SerializedName("total_size")
-//        private Long totalSize;
-//        @SerializedName("selected_size")
-//        private Long selectedSize;
-//        @SerializedName("selected_downloaded_size")
-//        private Long selectedDownloadedSize;
-//        @SerializedName("download_rate")
-//        private Long downloadRate;
-//        @SerializedName("upload_rate")
-//        private Long uploadRate;
-//        @SerializedName("error_code")
-//        private String errorCode;
-//        @SerializedName("error_message")
-//        private String errorMessage;
-//        @SerializedName("permillage")
-//        private Long permillage;
-//        @SerializedName("left_time")
-//        private String leftTime;
     }
 }
