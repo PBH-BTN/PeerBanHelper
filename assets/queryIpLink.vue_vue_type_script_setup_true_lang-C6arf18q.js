f5c8f17e4babdfaec5d24c7b2eb4bc82ee759a7a
Ghost-chu <Ghost-chu@users.noreply.github.com>
2025-03-01 10:38:33 +0000

    deploy: db251745cbdff78a812a810e79a069ff197c7591

---
+++ assets/queryIpLink.vue_vue_type_script_setup_true_lang-C6arf18q.js
@@ -0,0 +1,1 @@
+import{f as o,j as r,l as t,p as n,k as s}from"./libs-BMxpkVOl.js";const p=["href"],c=o({__name:"queryIpLink",props:{ip:{},style:{type:[Boolean,null,String,Object,Array]}},setup(a){return(e,l)=>(s(),r("a",{style:n([{"text-decoration":"none",cursor:"pointer"},e.style]),href:`ipHistory?ip=${e.ip}`},[t(e.$slots,"default")],12,p))}});export{c as _};
