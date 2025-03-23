c86d04fb1a3fa13d8feeed9397bd48f07beeb5f6
Ghost_chu <ghostchu@111.com>
2025-03-24 01:58:00 +0800

    更新 Portable 启动参数

--- pkg/portable/1) 以 GUI 模式启动.bat
+++ pkg/portable/1) 以 GUI 模式启动.bat
@@ -1,4 +1,4 @@
 @echo off
 chcp 65001
 title PeerBanHelper (GUI 模式)
-start ./jre/bin/javaw.exe -Xmx512M -Xss512k -XX:+UseG1GC -XX:+UseStringDeduplication -XX:+ShrinkHeapInSteps -Dpbh.release=portable -Dfile.encoding=UTF-8 -Dstdout.encoding=UTF-8 -Dstderr.encoding=UTF-8 -Dconsole.encoding=UTF-8 -jar PeerBanHelper.jar
+start ./jre/bin/javaw.exe -Xmx512M -Xss512k --enable-native-access=ALL-UNNAMED -XX:+UseG1GC -XX:+UseStringDeduplication -XX:+ShrinkHeapInSteps -Dpbh.release=portable -Dfile.encoding=UTF-8 -Dstdout.encoding=UTF-8 -Dstderr.encoding=UTF-8 -Dconsole.encoding=UTF-8 -jar PeerBanHelper.jar
