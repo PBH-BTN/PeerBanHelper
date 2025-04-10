58de4d8fee5e6c2fd7072d49f6cca7832b51485d
Ghost_chu <ghostchu@111.com>
2025-04-11 00:15:15 +0800

    修复 Portable 无法启动的问题

--- pkg/portable/2) 以 GUI 模式启动（静默启动到托盘图标）.bat
+++ pkg/portable/2) 以 GUI 模式启动（静默启动到托盘图标）.bat
@@ -1,4 +1,4 @@
 @echo off
 chcp 65001
 title PeerBanHelper (静默启动，GUI 模式)
-start ./jre/bin/javaw.exe -Xmx512M -Xss512k --enable-native-access=ALL-UNNAMED -XX:+UnlockExperimentalVMOptions -XX:+UseCompactObjectHeaders -XX:+UseG1GC -XX:+UseStringDeduplication -XX:+ShrinkHeapInSteps -Dpbh.release=portable -Dfile.encoding=UTF-8 -Dstdout.encoding=UTF-8 -Dstderr.encoding=UTF-8 -Dconsole.encoding=UTF-8 -jar PeerBanHelper.jar silent
+start ./jre/bin/javaw.exe -Xmx512M -Xss512k -XX:+UseG1GC -XX:+UseStringDeduplication -XX:+ShrinkHeapInSteps -Dpbh.release=portable -Dfile.encoding=UTF-8 -Dstdout.encoding=UTF-8 -Dstderr.encoding=UTF-8 -Dconsole.encoding=UTF-8 -jar PeerBanHelper.jar silent
--- pkg/portable/3. Console.bat
+++ pkg/portable/3. Console.bat
@@ -1,4 +1,4 @@
 @echo off
 title PeerBanHelper (Console)
-start ./jre/bin/java.exe -Xmx512M -Xss512k --enable-native-access=ALL-UNNAMED -XX:+UnlockExperimentalVMOptions -XX:+UseCompactObjectHeaders -XX:+UseG1GC -XX:+UseStringDeduplication -Djava.awt.headless=true -XX:+ShrinkHeapInSteps -Dpbh.release=portable -Dfile.encoding=UTF-8 -Dstdout.encoding=UTF-8 -Dstderr.encoding=UTF-8 -Dconsole.encoding=UTF-8 -jar PeerBanHelper.jar nogui
+start ./jre/bin/java.exe -Xmx512M -Xss512k -XX:+UseG1GC -XX:+UseStringDeduplication -Djava.awt.headless=true -XX:+ShrinkHeapInSteps -Dpbh.release=portable -Dfile.encoding=UTF-8 -Dstdout.encoding=UTF-8 -Dstderr.encoding=UTF-8 -Dconsole.encoding=UTF-8 -jar PeerBanHelper.jar nogui
 pause
