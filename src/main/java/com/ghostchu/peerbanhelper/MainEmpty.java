733818e1e33c81156c9e710d403ba42ee026529e
Ghost_chu <ghostchu@111.com>
2025-04-10 15:05:27 +0800

    清空代码

--- src/main/java/com/ghostchu/peerbanhelper/MainEmpty.java
+++ src/main/java/com/ghostchu/peerbanhelper/MainEmpty.java
@@ -1,20 +1,7 @@
 package com.ghostchu.peerbanhelper;
 
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
-
 public class MainEmpty {
     public static void main(String[] args) {
         // 什么都不做直接退出
-        var display = new Display();
-        var shell = new Shell();
-        shell.setText("test");
-        shell.setSize(512, 512);
-        shell.open();
-        while (!shell.isDisposed()) {
-            if (!display.readAndDispatch()) {
-                display.sleep();
-            }
-        }
     }
 }
