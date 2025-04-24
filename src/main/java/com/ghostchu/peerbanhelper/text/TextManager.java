dd07c9071b40b185eeea5dab5a0d4d013b9aa9b4
Ghost_chu <ghostchu@111.com>
2025-04-24 15:20:03 +0800

    fix warning

--- src/main/java/com/ghostchu/peerbanhelper/text/TextManager.java
+++ src/main/java/com/ghostchu/peerbanhelper/text/TextManager.java
@@ -75,7 +75,7 @@ public final class TextManager implements Reloadable {
         if (yamlConfiguration == null) {
             yamlConfiguration = INSTANCE_HOLDER.languageFilesManager.getDistribution("zh_cn");
             if (yamlConfiguration == null) {
-                log.warn("The locale {} are not supported and fallback locale en_us load failed.", locale);
+                log.warn("The locale {} are not supported and fallback locale zh_cn load failed.", locale);
                 return "Unsupported locale " + locale;
             }
         }
