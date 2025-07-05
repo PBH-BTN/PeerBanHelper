9f69fe17614be13bc4d811e115e45c8ab88b54fe
Ghost_chu <ghostchu@111.com>
2025-07-05 16:30:58 +0800

    添加排序参数databaseName与dtoName映射支持

--- src/main/java/com/ghostchu/peerbanhelper/util/query/Orderable.java
+++ src/main/java/com/ghostchu/peerbanhelper/util/query/Orderable.java
@@ -1,5 +1,7 @@
 package com.ghostchu.peerbanhelper.util.query;
 
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
 import com.j256.ormlite.stmt.QueryBuilder;
 import io.javalin.http.Context;
 
@@ -7,7 +9,8 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Orderable extends LinkedHashMap<String, Boolean> {
-
+    // databaseName -> dtoName
+    private final BiMap<String, String> dualDirectionMapping = HashBiMap.create();
     public Orderable(Map<String, Boolean> def, Context ctx) {
         if (ctx != null) {
             if (!ctx.queryParams("orderBy").isEmpty()) {
@@ -18,22 +21,35 @@ public class Orderable extends LinkedHashMap<String, Boolean> {
         appendOrders(def);
     }
 
-    private void appendOrders(String field, boolean asc) {
+    private Orderable appendOrders(String field, boolean asc) {
         put(field, asc);
+        return this;
     }
 
-    private void appendOrders(Map<String, Boolean> fields) {
+    private Orderable appendOrders(Map<String, Boolean> fields) {
         putAll(fields);
+        return this;
     }
 
-    private void appendOrders(Context ctx) {
+    private Orderable addMapping(String nameForDatabase, String nameForDto){
+        dualDirectionMapping.put(nameForDatabase, nameForDto);
+        return this;
+    }
+
+    private Orderable addMappings(Map<String, String> database2Dto){
+        dualDirectionMapping.putAll(database2Dto);
+        return this;
+    }
+
+    private Orderable appendOrders(Context ctx) { // here is dtoName...
         for (String orderBy : ctx.queryParams("orderBy")) {
             String[] spilt = orderBy.split("\\|");
             if (spilt.length == 0) continue;
-            String field = spilt[0];
+            String dtoName = spilt[0];
             boolean asc = spilt.length < 2 || !spilt[1].equalsIgnoreCase("desc");
-            put(field, asc);
+            put(dualDirectionMapping.inverse().getOrDefault(dtoName, dtoName), asc); // so we put the databaseName in orders
         }
+        return this;
     }
 
     public <T, ID> QueryBuilder<T, ID> apply(QueryBuilder<T, ID> queryBuilder) {
