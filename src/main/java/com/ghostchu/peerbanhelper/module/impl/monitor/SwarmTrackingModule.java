4efbfb7ede1f3e99ac487415601a668bd4963c0a
Ghost_chu <ghostchu@111.com>
2025-07-06 15:11:07 +0800

    默认启用 Swarm Tracking，注册 API 端点

--- src/main/java/com/ghostchu/peerbanhelper/module/impl/monitor/SwarmTrackingModule.java
+++ src/main/java/com/ghostchu/peerbanhelper/module/impl/monitor/SwarmTrackingModule.java
@@ -8,6 +8,11 @@ import com.ghostchu.peerbanhelper.database.table.tmp.TrackedSwarmEntity;
 import com.ghostchu.peerbanhelper.downloader.Downloader;
 import com.ghostchu.peerbanhelper.module.AbstractFeatureModule;
 import com.ghostchu.peerbanhelper.module.MonitorFeatureModule;
+import com.ghostchu.peerbanhelper.util.query.Pageable;
+import com.ghostchu.peerbanhelper.web.JavalinWebContainer;
+import com.ghostchu.peerbanhelper.web.Role;
+import com.ghostchu.peerbanhelper.web.wrapper.StdResp;
+import io.javalin.http.Context;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +20,9 @@ import org.springframework.stereotype.Component;
 
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 
 @Slf4j
@@ -24,6 +31,8 @@ import java.util.concurrent.ExecutorService;
 public final class SwarmTrackingModule extends AbstractFeatureModule implements MonitorFeatureModule {
     @Autowired
     private TrackedSwarmDao trackedSwarmDao;
+    @Autowired
+    private JavalinWebContainer javalinWebContainer;
 
     /**
      * 如果返回 false，则不初始化任何配置文件相关对象
@@ -61,6 +70,33 @@ public final class SwarmTrackingModule extends AbstractFeatureModule implements 
     @Override
     public void onEnable() {
         Main.getEventBus().register(this);
+        javalinWebContainer.javalin()
+                .get("/api/modules/" + getConfigName(), this::handleWebAPI, Role.USER_READ);
+        javalinWebContainer.javalin()
+                .get("/api/modules/" + getConfigName() + "/details", this::handleDetails, Role.USER_READ);
+    }
+
+    private void handleDetails(@NotNull Context context) {
+        Pageable pageable = new Pageable(context);
+        try {
+            var page = trackedSwarmDao.queryByPaging(pageable);
+            context.json(new StdResp(true, null, page));
+        } catch (SQLException e) {
+            log.error("Unable to retrieve tracked swarm data", e);
+            context.json(new StdResp(false, "Unable to retrieve tracked swarm data", null));
+        }
+    }
+
+    private void handleWebAPI(@NotNull Context context) {
+        Map<String, Object> response = new HashMap<>();
+        try {
+            response.put("trackedSwarmSize", trackedSwarmDao.countOf());
+            context.json(response);
+        } catch (SQLException e) {
+            log.error("Unable to retrieve tracked swarm data", e);
+            context.status(500).json(new StdResp(false, "Unable to retrieve tracked swarm data", null));
+            return;
+        }
     }
 
     /**
