c89070604fb0c1616df41a19bec8c8a23796e35e
Gaojianli <admin@gaojianli.me>
2025-07-14 13:24:14 +0800

    remove http2

--- webui/src/components/forms/qbittorrent.vue
+++ webui/src/components/forms/qbittorrent.vue
@@ -35,13 +35,6 @@
       <a-input-password v-model="config.basicAuth.pass" />
     </a-form-item>
   </a-form-item>
-  <a-form-item field="config.httpVersion" :label="t('page.dashboard.editModal.label.httpVersion')">
-    <a-radio-group v-model="config.httpVersion">
-      <a-radio value="HTTP_1_1">1.1</a-radio>
-      <a-radio value="HTTP_2">2.0</a-radio>
-    </a-radio-group>
-    <template #extra>{{ t('page.dashboard.editModal.label.httpVersion.description') }} </template>
-  </a-form-item>
   <a-form-item
     field="config.incrementBan"
     default-checked
