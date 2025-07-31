04f581c6d392b721c9f82f89b19cd5d163ff3045
Ghost_chu <ghostchu@111.com>
2025-07-31 19:08:38 +0800

    更新内存配置参数

--- pkg/portable/1) 以 GUI 模式启动.bat
+++ pkg/portable/1) 以 GUI 模式启动.bat
@@ -1,4 +1,4 @@
 @echo off
 chcp 65001
 title PeerBanHelper (GUI 模式)
-start ./jre/bin/javaw.exe -XX:+UseZGC -XX:+ZGenerational -XX:SoftMaxHeapSize=256M -XX:ZUncommitDelay=1 -Xss512k -XX:+UseStringDeduplication -XX:+ShrinkHeapInSteps -Dsun.net.useExclusiveBind=false -Dpbh.release=portable -Dfile.encoding=UTF-8 -Dstdout.encoding=UTF-8 -Dstderr.encoding=UTF-8 -Dconsole.encoding=UTF-8 -jar PeerBanHelper.jar
+start ./jre/bin/javaw.exe -XX:+UseZGC -XX:+ZGenerational -XX:SoftMaxHeapSize=386M -XX:ZUncommitDelay=1 -Xss512k -XX:+UseStringDeduplication -XX:+ShrinkHeapInSteps -Dsun.net.useExclusiveBind=false -Dpbh.release=portable -Dfile.encoding=UTF-8 -Dstdout.encoding=UTF-8 -Dstderr.encoding=UTF-8 -Dconsole.encoding=UTF-8 -jar PeerBanHelper.jar
--- pkg/portable/3) 以控制台模式启动.bat
+++ pkg/portable/3) 以控制台模式启动.bat
@@ -1,5 +1,5 @@
 @echo off
 chcp 65001
 title PeerBanHelper (控制台模式)
-start ./jre/bin/java.exe -XX:+UseZGC -XX:+ZGenerational -XX:SoftMaxHeapSize=256M -XX:ZUncommitDelay=1 -Xss512k -XX:+UseStringDeduplication -XX:+ShrinkHeapInSteps -Dsun.net.useExclusiveBind=false -Dpbh.release=portable -Dfile.encoding=UTF-8 -Dstdout.encoding=UTF-8 -Dstderr.encoding=UTF-8 -Dconsole.encoding=UTF-8 -jar PeerBanHelper.jar nogui
+start ./jre/bin/java.exe -XX:+UseZGC -XX:+ZGenerational -XX:SoftMaxHeapSize=386M -XX:ZUncommitDelay=1 -Xss512k -XX:+UseStringDeduplication -XX:+ShrinkHeapInSteps -Dsun.net.useExclusiveBind=false -Dpbh.release=portable -Dfile.encoding=UTF-8 -Dstdout.encoding=UTF-8 -Dstderr.encoding=UTF-8 -Dconsole.encoding=UTF-8 -jar PeerBanHelper.jar nogui
 pause
