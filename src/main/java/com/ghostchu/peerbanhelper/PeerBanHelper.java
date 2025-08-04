22fe3def38fdc75902783792903ce901f212df3e
Ghost_chu <ghostchu@111.com>
2025-08-04 18:29:57 +0800

    禁用 MCP 模块，因为没做完

--- src/main/java/com/ghostchu/peerbanhelper/PeerBanHelper.java
+++ src/main/java/com/ghostchu/peerbanhelper/PeerBanHelper.java
@@ -9,7 +9,6 @@ import com.ghostchu.peerbanhelper.event.PBHServerStartedEvent;
 import com.ghostchu.peerbanhelper.exchange.ExchangeMap;
 import com.ghostchu.peerbanhelper.gui.TaskbarState;
 import com.ghostchu.peerbanhelper.module.ModuleManager;
-import com.ghostchu.peerbanhelper.module.impl.ai.mcp.MCPController;
 import com.ghostchu.peerbanhelper.module.impl.background.BackgroundModule;
 import com.ghostchu.peerbanhelper.module.impl.monitor.ActiveMonitoringModule;
 import com.ghostchu.peerbanhelper.module.impl.monitor.SwarmTrackingModule;
@@ -297,7 +296,7 @@ public class PeerBanHelper implements Reloadable {
         moduleManager.register(PBHUtilitiesController.class);
         moduleManager.register(BackgroundModule.class);
         moduleManager.register(SwarmTrackingModule.class);
-        moduleManager.register(MCPController.class);
+        // moduleManager.register(MCPController.class);
         moduleManager.register(PBHAutoStunController.class);
     }
 
