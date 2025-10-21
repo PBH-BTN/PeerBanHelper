889af5978f3d3951e2859adae74629147441b6ae
Gaojianli <admin@gaojianli.me>
2025-10-21 15:15:13 +0800

    fix plus chart style

--- webui/src/views/charts/components/plusWarpper.vue
+++ webui/src/views/charts/components/plusWarpper.vue
@@ -1,18 +1,20 @@
 <template>
   <slot v-if="endpointStore.plusStatus?.enabledFeatures?.includes('basic')"></slot>
-  <a-card v-else hoverable :title="title">
-    <a-result class="overlay" status="warning" :title="t('page.charts.locked')">
-      <template #icon>
-        <icon-lock />
-      </template>
-      <template #subtitle> {{ t('page.charts.locked.tips') }} </template>
-      <template #extra>
-        <a-button type="primary" @click="activeSubscription">{{
-          t('page.charts.locked.active')
-        }}</a-button>
-      </template>
-    </a-result>
-    <dummyChart />
+  <a-card v-else hoverable :title="title" class="card-container">
+    <div class="card-body-wrapper">
+      <a-result class="overlay" status="warning" :title="t('page.charts.locked')">
+        <template #icon>
+          <icon-lock />
+        </template>
+        <template #subtitle> {{ t('page.charts.locked.tips') }} </template>
+        <template #extra>
+          <a-button type="primary" @click="activeSubscription">{{
+            t('page.charts.locked.active')
+          }}</a-button>
+        </template>
+      </a-result>
+      <dummyChart />
+    </div>
   </a-card>
 </template>
 <script setup lang="ts">
@@ -30,7 +32,21 @@ const activeSubscription = () => {
   endpointStore.emitter.emit('open-plus-modal')
 }
 </script>
-<style scoped>
+<style scoped lang="less">
+.card-container {
+  position: relative;
+
+  // 消除 arco-card-body 的 padding
+  :deep(.arco-card-body) {
+    padding: 0;
+  }
+}
+
+.card-body-wrapper {
+  position: relative;
+  padding: 16px;
+}
+
 .overlay {
   position: absolute;
   top: 0;
@@ -50,6 +66,7 @@ const activeSubscription = () => {
   justify-content: center;
   text-align: center;
 }
+
 body[arco-theme='dark'] .overlay {
   background: rgba(0, 0, 0, 0.5); /* 深色模式下的半透明背景 */
   color: #fff; /* 深色模式下的文本颜色 */
