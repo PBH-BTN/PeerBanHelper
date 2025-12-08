bd77c51c55f5acbbfcf00a4835462e74e9a9fa18
Gaojianli <admin@gaojianli.me>
2025-12-08 21:52:36 +0800

    fix oobe center

--- webui/src/views/oobe/components/result.vue
+++ webui/src/views/oobe/components/result.vue
@@ -1,12 +1,7 @@
 <template>
-  <a-spin v-if="loading" class="center" :tip="t('page.oobe.result.initlizing')" />
-  <div v-else>
-    <a-result
-      v-if="initSuccess"
-      status="success"
-      :title="t('page.oobe.result.title')"
-      class="center"
-    >
+  <div class="result-container">
+    <a-spin v-if="loading" :tip="t('page.oobe.result.initlizing')" />
+    <a-result v-else-if="initSuccess" status="success" :title="t('page.oobe.result.title')">
       <template #subtitle> {{ t('page.oobe.result.description') }} </template>
       <template #extra>
         <a-space>
@@ -14,7 +9,7 @@
         </a-space>
       </template>
     </a-result>
-    <a-result v-else status="error" :title="t('page.oobe.result.title.error')" class="center">
+    <a-result v-else status="error" :title="t('page.oobe.result.title.error')">
       <template #subtitle> {{ errorMsg }} </template>
       <template #extra>
         <a-space>
@@ -27,10 +22,10 @@
 <script lang="ts" setup>
 import type { InitConfig } from '@/api/model/oobe'
 import { InitPBH } from '@/service/init'
-import { ref } from 'vue'
-import { useI18n } from 'vue-i18n'
 import { useEndpointStore } from '@/stores/endpoint'
 import { v1 as uuid } from 'uuid'
+import { ref } from 'vue'
+import { useI18n } from 'vue-i18n'
 const { t } = useI18n()
 const config = defineModel<InitConfig>({ required: true })
 const loading = ref(true)
@@ -64,10 +59,13 @@ init()
 </script>
 
 <style lang="less" scoped>
-.center {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+.result-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  margin-top: 100px;
+  min-height: 300px;
 }
 </style>
