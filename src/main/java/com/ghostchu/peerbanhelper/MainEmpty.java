489dc61444f5564dfbf087302fc2b937194aab1a
Ghost_chu <ghostchu@111.com>
2025-12-09 16:03:30 +0800

    删除多余的代码

--- src/main/java/com/ghostchu/peerbanhelper/MainEmpty.java
+++ src/main/java/com/ghostchu/peerbanhelper/MainEmpty.java
@@ -1,11 +1,7 @@
 package com.ghostchu.peerbanhelper;
 
-import com.ghostchu.peerbanhelper.util.InfoHashUtil;
-
 public final class MainEmpty {
     public static void main(String[] args) {
         // 什么都不做直接退出
-        var id = InfoHashUtil.getHashedIdentifier("0be30e855da0076e8fa2a7f486c96d6ab2cf0e54");
-        System.out.println(id);
     }
 }
