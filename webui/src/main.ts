02e5e37d13c677772404605036fff775bf62628d
Gaojianli <admin@gaojianli.me>
2026-01-21 15:46:27 +0800

    总之先加上Sentry

--- webui/src/main.ts
+++ webui/src/main.ts
@@ -1,4 +1,5 @@
 import { Message, Modal, Notification } from '@arco-design/web-vue'
+import * as Sentry from '@sentry/vue'
 import 'normalize.css'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
@@ -7,7 +8,6 @@ import App from './App.vue'
 import './assets/main.less'
 import i18n from './locale'
 import router from './router'
-
 const app = createApp(App)
 Message._context = app._context
 Notification._context = app._context
@@ -17,9 +17,35 @@ setGlobalOptions({
   loadingKeep: 1000,
   pollingWhenOffline: true
 })
-
-app.use(createPinia())
+const pinia = createPinia()
+app.use(pinia)
 app.use(i18n)
 app.use(router)
-
+if (process.env.SENTRY_DSN) {
+  Sentry.init({
+    app,
+    dsn: process.env.SENTRY_DSN,
+    enabled: false,
+    tracesSampleRate: 0.01,
+    integrations: [
+      Sentry.browserTracingIntegration({ router }),
+      Sentry.vueIntegration({
+        tracingOptions: {
+          trackComponents: true
+        }
+      }),
+      Sentry.browserApiErrorsIntegration({
+        setTimeout: true,
+        setInterval: true,
+        requestAnimationFrame: true,
+        XMLHttpRequest: true,
+        eventTarget: true,
+        unregisterOriginalCallbacks: true
+      })
+    ],
+    environment: process.env.NODE_ENV === 'production' ? 'prod' : 'dev',
+    release: __APP_VERSION__
+  })
+  pinia.use(Sentry.createSentryPiniaPlugin())
+}
 app.mount('#app')
