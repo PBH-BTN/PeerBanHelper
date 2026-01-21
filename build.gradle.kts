14d8839334caf69ec5a64938655102dc0bb1ae4c
Ghost_chu <ghostchu@111.com>
2026-01-21 16:03:38 +0800

    包含上

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,12 @@ java {
     targetCompatibility = JavaVersion.VERSION_25
 }
 
+sentry {
+    includeSourceContext.set(true)
+    includeNativeSources.set(true)
+    includeDependenciesReport.set(true)
+}
+
 repositories {
     mavenCentral()
     maven {
