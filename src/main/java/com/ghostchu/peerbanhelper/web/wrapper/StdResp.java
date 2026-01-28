855a3b99243de5d6ee26adb4215100e0c95aba26
Ghost_chu <ghostchu@111.com>
2026-01-29 00:56:39 +0800

    移除没用的 AsyncTaskManager

--- src/main/java/com/ghostchu/peerbanhelper/web/wrapper/StdResp.java
+++ src/main/java/com/ghostchu/peerbanhelper/web/wrapper/StdResp.java
@@ -1,30 +1,23 @@
 package com.ghostchu.peerbanhelper.web.wrapper;
 
-import com.ghostchu.peerbanhelper.util.asynctask.AsyncTask;
 import lombok.Data;
 import org.jetbrains.annotations.Nullable;
 
 @Data
 public class StdResp {
     private final boolean success;
-    private final @Nullable AsyncTaskDTO asyncTask;
     private final @Nullable String message;
     private final @Nullable Object data;
 
-    public StdResp(boolean success, @Nullable String message, @Nullable Object data){
+    public StdResp(boolean success, @Nullable String message, @Nullable Object data) {
         this.success = success;
-        this.asyncTask = null;
         this.message = message;
         this.data = data;
     }
 
-    public StdResp(boolean success, @Nullable String message, boolean showLogs, AsyncTask task) {
+    public StdResp(boolean success, @Nullable String message, boolean showLogs) {
         this.success = success;
-        this.asyncTask = new AsyncTaskDTO(task.getTaskId(), showLogs);
         this.message = message;
         this.data = null;
-    }
-
-    public record AsyncTaskDTO(String id, boolean showLogs) {
     }
 }
