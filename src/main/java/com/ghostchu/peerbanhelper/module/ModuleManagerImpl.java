e87a4a77ba82037ddbe43022c3e681dc7bcce224
Ghost_chu <ghostchu@111.com>
2026-01-29 19:03:08 +0800

    使用并发模块加载

--- src/main/java/com/ghostchu/peerbanhelper/module/ModuleManagerImpl.java
+++ src/main/java/com/ghostchu/peerbanhelper/module/ModuleManagerImpl.java
@@ -11,6 +11,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Slf4j
