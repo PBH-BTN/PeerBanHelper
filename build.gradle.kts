305cba3cedc5fa395e12c865f012b8e45d8fc0b5
Ghost_chu <ghostchu@111.com>
2026-01-31 22:52:10 +0800

    修复运行时NoClassDefError

--- build.gradle.kts
+++ build.gradle.kts
@@ -139,6 +139,7 @@ dependencies {
 
     // Logging
     implementation("ch.qos.logback:logback-classic:1.5.26")
+    implementation("org.slf4j:jcl-over-slf4j:2.0.17")
 
     // Async utilities
     implementation("com.spotify:completable-futures:0.3.6")
