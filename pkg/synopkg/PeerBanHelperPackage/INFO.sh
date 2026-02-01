ffa333baa2c3bdf6119304b16059e6fa16003d51
Ghost_chu <ghostchu@111.com>
2026-02-01 19:47:34 +0800

    添加 -1 以便 SynoRepo 识别

--- pkg/synopkg/PeerBanHelperPackage/INFO.sh
+++ pkg/synopkg/PeerBanHelperPackage/INFO.sh
@@ -4,7 +4,7 @@
 source /pkgscripts/include/pkg_util.sh
 
 package="peerbanhelper"
-version="@PBH_VERSION@"
+version="@PBH_VERSION@-1"
 displayname="PeerBanHelper"
 os_min_ver="7.2-64555"
 maintainer="PBH-BTN"
