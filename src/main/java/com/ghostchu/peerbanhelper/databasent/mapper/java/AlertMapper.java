95452d4e05345e62aeddda2a6b1016b5d5e8632b
Ghost_chu <ghostchu@111.com>
2026-02-09 00:50:14 +0800

    使用 @Update 注解

--- src/main/java/com/ghostchu/peerbanhelper/databasent/mapper/java/AlertMapper.java
+++ src/main/java/com/ghostchu/peerbanhelper/databasent/mapper/java/AlertMapper.java
@@ -4,14 +4,15 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.ghostchu.peerbanhelper.databasent.table.AlertEntity;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.jetbrains.annotations.NotNull;
 
 import java.time.OffsetDateTime;
 
 public interface AlertMapper extends BaseMapper<AlertEntity> {
-    @Select("UPDATE alert SET read_at = #{readAt} WHERE read_at IS NULL")
+    @Update("UPDATE alert SET read_at = #{readAt} WHERE read_at IS NULL")
     void markAllAsRead(@NotNull OffsetDateTime readAt);
 
-    @Select("UPDATE alert SET read_at = #{readAt} WHERE identifier = #{identifier} AND read_at IS NULL")
+    @Update("UPDATE alert SET read_at = #{readAt} WHERE identifier = #{identifier} AND read_at IS NULL")
     void markAsRead(@NotNull @Param("identifier") String identifier, @NotNull OffsetDateTime readAt);
 }
