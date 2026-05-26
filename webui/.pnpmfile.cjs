beb15361fec39c46bace01f742830c1c6ea4c88f
Gaojianli <admin@gaojianli.me>
2026-05-26 20:42:00 +0800

    切换到ox，提升lint 性能100倍

--- webui/.pnpmfile.cjs
+++ webui/.pnpmfile.cjs
@@ -1,7 +1,7 @@
 /**
  * This hook is called after all packages have been resolved.
  * To remove the tarball URLs from the lockfile.
- * 
+ *
  * Because the pnpm config lockfile-include-tarball-url cannot work,
  * we need to remove the tarball URLs from the lockfile manually.
  * @see https://github.com/pnpm/pnpm/issues/6667#issuecomment-2971121163
@@ -10,14 +10,14 @@ function afterAllResolved(lockfile) {
   // Remove the tarball URLs from the lockfile
   for (const key in lockfile.packages) {
     if (lockfile.packages[key].resolution?.tarball) {
-      delete lockfile.packages[key].resolution.tarball;
+      delete lockfile.packages[key].resolution.tarball
     }
   }
-  return lockfile;
+  return lockfile
 }
 
 module.exports = {
   hooks: {
-    afterAllResolved,
-  },
-};
+    afterAllResolved
+  }
+}
