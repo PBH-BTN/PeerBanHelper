fa555b36c54f756fc4a6a4602741a03eb7333c48
paulzzh <28055560+paulzzh@users.noreply.github.com>
2026-07-12 02:37:31 +0800

    不要清理

--- pkg/synopkg/deploy-env.sh
+++ pkg/synopkg/deploy-env.sh
@@ -11,8 +11,11 @@ cp include/python/pkgdeploy.py pkgdeploy.py.bak
 sed -i 's|self.adjust_chroot(platform)|continue|g' include/python/pkgdeploy.py
 
 ./EnvDeploy -v 7.2 -p apollolake
+chroot /toolkit/build_env/ds.apollolake-7.2 umount /proc
+
 cp pkgdeploy.py.bak include/python/pkgdeploy.py
 
+sed -i 's|toolkit.clean()|#toolkit.clean()|g' EnvDeploy
 sed -i '/self.__check_tarball_exists()/d' include/python/pkgdeploy.py
 sed -i '/self.deploy_base_env(platform)/d' include/python/pkgdeploy.py
 sed -i '/self.deploy_env(platform)/d' include/python/pkgdeploy.py
