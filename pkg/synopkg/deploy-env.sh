258558877434bad1882e441961de2917d4ac1c96
paulzzh <28055560+paulzzh@users.noreply.github.com>
2026-07-12 01:31:15 +0800

    优化 spk 缓存

--- pkg/synopkg/deploy-env.sh
+++ pkg/synopkg/deploy-env.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-mkdir -p /toolkit
 cd /toolkit || exit
 git clone https://github.com/SynologyOpenSource/pkgscripts-ng
 cd /toolkit/pkgscripts-ng/ || exit
 git checkout DSM7.2
+
+cp include/python/pkgdeploy.py pkgdeploy.py.bak
+
+sed -i 's|self.adjust_chroot(platform)|continue|g' include/python/pkgdeploy.py
+
 ./EnvDeploy -v 7.2 -p apollolake
-chroot /toolkit/build_env/ds.apollolake-7.2 umount /proc
+
+cp pkgdeploy.py.bak include/python/pkgdeploy.py
