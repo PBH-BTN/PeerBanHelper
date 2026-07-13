baa080a22fd483858a6742fde8031f72ec4dec52
paulzzh <28055560+paulzzh@users.noreply.github.com>
2026-07-13 12:22:16 +0800

    再次尝试修复 mysql 建表

--- src/main/resources/db/migration/mysql/V1_3__remove_unique_index_for_peer_records_port.sql
+++ src/main/resources/db/migration/mysql/V1_3__remove_unique_index_for_peer_records_port.sql
@@ -1,16 +1,25 @@
-DELETE pr1
-FROM peer_records pr1
-JOIN peer_records pr2
-  ON pr2.address = pr1.address
- AND pr2.torrent_id = pr1.torrent_id
- AND pr2.downloader = pr1.downloader
- AND (
-      pr2.last_time_seen > pr1.last_time_seen
-      OR (
-          pr2.last_time_seen = pr1.last_time_seen
-          AND pr2.id > pr1.id
-      )
- );
+DELETE FROM peer_records
+WHERE id IN (
+    SELECT id
+    FROM (
+             SELECT pr1.id
+             FROM peer_records pr1
+             WHERE EXISTS (
+                 SELECT 1
+                 FROM peer_records pr2
+                 WHERE pr2.address = pr1.address
+                   AND pr2.torrent_id = pr1.torrent_id
+                   AND pr2.downloader = pr1.downloader
+                   AND (
+                     pr2.last_time_seen > pr1.last_time_seen
+                         OR (
+                         pr2.last_time_seen = pr1.last_time_seen
+                             AND pr2.id > pr1.id
+                         )
+                     )
+             )
+         ) AS duplicates
+);
 
-ALTER TABLE peer_records DROP CONSTRAINT IF EXISTS idx_peer_records_unique;
+ALTER TABLE peer_records DROP INDEX;
 ALTER TABLE peer_records ADD CONSTRAINT idx_peer_records_unique UNIQUE (address, torrent_id, downloader);
