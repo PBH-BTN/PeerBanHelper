a1996727d444b942d78b7f20c4b3c6cfde45e3f3
Ghost_chu <ghostchu@111.com>
2026-07-29 17:25:39 +0800

    测试期间换成包装类型

--- src/main/java/com/ghostchu/peerbanhelper/downloader/impl/aria2next/bean/A2Peer.java
+++ src/main/java/com/ghostchu/peerbanhelper/downloader/impl/aria2next/bean/A2Peer.java
@@ -14,47 +14,47 @@ import org.jetbrains.annotations.Nullable;
 public class A2Peer implements Peer {
 
     @JsonProperty("amChoking")
-    private boolean amChoking;
+    private Boolean amChoking;
     @JsonProperty("amInterested")
-    private boolean amInterested;
+    private Boolean amInterested;
     @JsonProperty("bitfield")
     private String bitfield;
     @JsonProperty("completedLength")
-    private long completedLength;
+    private Long completedLength;
     @JsonProperty("downloadSpeed")
-    private long downloadSpeed;
+    private Long downloadSpeed;
     @JsonProperty("downloaded")
-    private long downloaded;
+    private Long downloaded;
     @JsonProperty("flags")
     private String flags;
     @JsonProperty("handshaking")
-    private boolean handshaking;
+    private Boolean handshaking;
     @JsonProperty("incoming")
-    private boolean incoming;
+    private Boolean incoming;
     @JsonProperty("ip")
     private String ip;
     @JsonProperty("optimisticUnchoke")
-    private boolean optimisticUnchoke;
+    private Boolean optimisticUnchoke;
     @JsonProperty("peerChoking")
-    private boolean peerChoking;
+    private Boolean peerChoking;
     @JsonProperty("peerClientName")
     private String peerClientName;
     @JsonProperty("peerId")
     private String peerId;
     @JsonProperty("peerInterested")
-    private boolean peerInterested;
+    private Boolean peerInterested;
     @JsonProperty("port")
-    private int port;
+    private Integer port;
     @JsonProperty("progress")
-    private double progress;
+    private Double progress;
     @JsonProperty("seeder")
-    private boolean seeder;
+    private Boolean seeder;
     @JsonProperty("snubbed")
-    private boolean snubbed;
+    private Boolean snubbed;
     @JsonProperty("uploadSpeed")
-    private long uploadSpeed;
+    private Long uploadSpeed;
     @JsonProperty("uploaded")
-    private long uploaded;
+    private Long uploaded;
     private transient PeerAddress peerAddress;
 
     @Override
@@ -83,5 +83,36 @@ public class A2Peer implements Peer {
                 .snubbed(snubbed)
                 .seed(seeder)
                 .build();
+    }
+
+    @Override
+    public boolean isHandshaking() {
+        return handshaking != null && handshaking;
+    }
+
+    @Override
+    public double getProgress() {
+        return progress != null ? progress : -1;
+    }
+
+
+    @Override
+    public long getDownloadSpeed() {
+        return downloadSpeed != null ? downloadSpeed : -1;
+    }
+
+    @Override
+    public long getDownloaded() {
+        return downloaded != null ? downloaded : -1;
+    }
+
+    @Override
+    public long getUploadSpeed() {
+        return uploadSpeed != null ? uploadSpeed : -1;
+    }
+
+    @Override
+    public long getUploaded() {
+        return uploaded != null ? uploaded : -1;
     }
 }
